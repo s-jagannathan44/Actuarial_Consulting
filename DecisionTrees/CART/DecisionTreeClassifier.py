a77633767c95944b807c24496a3bf84f5beb6d90
s-jagannathan44 <85141510+s-jagannathan44@users.noreply.github.com>
2022-09-22 11:28:42 +0530

    3rd change to incorporate Column transformer

--- DecisionTrees/CART/DecisionTreeClassifier.py
+++ DecisionTrees/CART/DecisionTreeClassifier.py
@@ -1,6 +1,8 @@
 import pandas
 import pandas as pd
 import matplotlib.pyplot as plt
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import OneHotEncoder, OrdinalEncoder
 from sklearn.tree import DecisionTreeClassifier, plot_tree
 from sklearn.model_selection import train_test_split
 
@@ -10,21 +12,30 @@ df = pd.read_csv("Output\\Input_3.csv")
 for c in df.columns:
     print(df[c].name, df[c].unique())
 X = df.drop('Claim', axis=1)
+columns = X.columns
 y = df["Claim"]
-X['GenderMainDriver'] = X['GenderMainDriver'].apply(lambda x: 0 if x == 'Male' else 1)
-X['VehFuel1'] = X['VehFuel1'].apply(lambda x: 0.5 if x == 'D' else -0.5)
-X = pd.get_dummies(X, columns=['MaritalMainDriver', 'DrivingRestriction'])
+transformer = ColumnTransformer(
+    [
+        (
+            "onehot_categorical",
+            OneHotEncoder(),
+            ["MaritalMainDriver",  "DrivingRestriction"],
+        ),
+(
+            "ordinal",
+            OrdinalEncoder(),
+            ["GenderMainDriver",  "VehFuel1"],
+        ),
+    ],
+    remainder='drop'
+)
 
-
+X = transformer.fit_transform(X)
 
 # Step2 Build a preliminary Classification tree
 
 # default test size is test_size=0.25
 X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=40)
-
-frame = pandas.DataFrame(X_test.copy(), columns=X.columns)
-frame["Claim"] = y_test
-
 
 # we can add min_impurity_decrease, class_weight and max_depth parameters here
 clf_dt = DecisionTreeClassifier(random_state=42)
@@ -32,12 +43,20 @@ clf_dt = clf_dt.fit(X_train, y_train)
 
 # We will plot the tree here
 plt.figure(figsize=(15, 7.5))
-plot_tree(clf_dt, filled=True, rounded=True, class_names=["No Claim", "Claim"], feature_names=X.columns)
+plot_tree(clf_dt, filled=True, rounded=True, class_names=["No Claim", "Claim"])
 plt.show()
 
-threshold = 0.033
-# y_pred = (clf_dt.predict_proba(X_test)[:, 1] >= threshold).astype(bool)  # set threshold as 0.3
 y_pred = clf_dt.predict_proba(X_test)
-Output_frame = frame.copy()
-Output_frame['Predicted'] = y_pred[:, 1:]
-Output_frame.to_csv("Output\\Output.csv")
+
+one_hot = transformer.named_transformers_['onehot_categorical'].inverse_transform(X_test[:, :9])
+ordinal = transformer.named_transformers_['ordinal'].inverse_transform(X_test[:, 9:])
+one_frame = pandas.DataFrame(one_hot, columns=["MaritalMainDriver",  "DrivingRestriction"])
+ordinal_frame = pandas.DataFrame(ordinal, columns=["GenderMainDriver",  "VehFuel1"])
+
+# axis 0 is vertical and axis 1 is horizontal
+output = pd.concat([one_frame, ordinal_frame], axis=1)
+
+frame = pandas.DataFrame(output.copy(), columns=columns)
+frame["Claim"] = y_test.to_list()
+frame['Predicted'] = y_pred[:, 1:]
+frame.to_csv("Output\\Output.csv")
