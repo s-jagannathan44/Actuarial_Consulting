1ba9ea1da7882a4ce384e924b1c1038f7d33615a
s-jagannathan44 <85141510+s-jagannathan44@users.noreply.github.com>
2022-09-29 13:16:35 +0530

    Baseline weight model

--- DecisionTrees/CART/DecisionTreeRegressor.py
+++ DecisionTrees/CART/DecisionTreeRegressor.py
@@ -33,7 +33,7 @@ def calculate_alpha(X_val, y_val, tree_):
     return frame["alpha"].iloc[0]
 
 
-def return_best_tree(tree_, X_train_val, y_train_val, y_test_val):
+def return_best_tree(tree_, X_train_val, y_train_val, y_test_val, exposure):
     # get the mean baseline because this is a regression problem
     # with regression, the baseline can be as simple as the mean.
     mean_baseline = y_test_val.mean()
@@ -47,10 +47,12 @@ def return_best_tree(tree_, X_train_val, y_train_val, y_test_val):
     param_grid = {
         'max_depth': [3, 4, 5, 6],
         'min_samples_leaf': [100, 20, 1],
+        'min_weight_fraction_leaf': [0.15, 0.25, 0.45]
     }
+    param_dict = {'sample_weight': exposure}
     mae_scorer = make_scorer(mean_absolute_error, greater_is_better=False)
-    tree_reg = GridSearchCV(tree_, param_grid, scoring=mae_scorer, cv=5, refit=True, verbose=3, n_jobs=-1)
-    tree_reg.fit(X_train_val, y_train_val)
+    tree_reg = GridSearchCV(tree_, param_grid, scoring=mae_scorer, cv=10, refit=True, verbose=3, n_jobs=-1)
+    tree_reg.fit(X_train_val, y_train_val, **param_dict)
     # print best parameter after tuning
     print(tree_reg.best_params_)
     return tree_reg.best_estimator_
@@ -78,7 +80,7 @@ def get_columns():
     return columns
 
 
-def write_output(X_val, actual_val, pred_val):
+def write_output(X_val, actual_val, pred_val, exposure):
     one_hot = transformer.named_transformers_['onehot_categorical'].inverse_transform(X_val[:, ord_col_size:])
     ordinal = transformer.named_transformers_['ordinal'].inverse_transform(X_val[:, :ord_col_size])
     one_frame = pd.DataFrame(one_hot, columns=["MaritalMainDriver", "DrivingRestriction", "Make"])
@@ -86,6 +88,7 @@ def write_output(X_val, actual_val, pred_val):
 
     # axis 0 is vertical and axis 1 is horizontal
     output = pd.concat([one_frame, ordinal_frame], axis=1)
+    output["Exposure"] = exposure
     frame = pd.DataFrame(output.copy(), columns=df.columns)
     frame["Claim"] = actual_val.to_list()
     frame['Predicted'] = pred_val
@@ -103,6 +106,7 @@ def read_analyze_transform(filename):
 
     transformer_ = ColumnTransformer(
         [
+            ("passthrough_numeric", "passthrough", ["Exposure"]),
             ("ordinal", OrdinalEncoder(), ["GenderMainDriver", "VehFuel1"]),
             ("onehot_categorical", OneHotEncoder(), ["MaritalMainDriver", "DrivingRestriction", "Make"],),
         ],
@@ -112,11 +116,14 @@ def read_analyze_transform(filename):
     return df_, X_, y_, transformer_
 
 
-def build_tree(X_val, y_val, y_test_val, alpha):
+def build_tree(X_val, y_val, alpha):
     dt_ = DecisionTreeRegressor(random_state=42, criterion='absolute_error', ccp_alpha=alpha)
-    dt_.fit(X_val, y_val)
+    flat_arr = X_val[:, :1]
+    exposure = np.reshape(flat_arr, np.shape(X_val)[0])
+    X_val = X_val[:, 1:]
+    dt_.fit(X_val, y_val, sample_weight=exposure)
     # We will export the tree here
-    dt_ = return_best_tree(dt_, X_val, y_val, y_test_val)
+    dt_ = return_best_tree(dt_, X_val, y_val, y_val, exposure)
     tree.export_graphviz(dt_, out_file="Output\\tree.img", filled=True, rounded=True, feature_names=get_columns())
     joblib.dump(dt_, "SeverityRegressionTree.sav")
     return dt_
@@ -135,20 +142,26 @@ def predict_tree(X_val, y_val, tree_):
 
 # -------------------- CODE STARTS HERE ---------------------------------------
 ord_col_size = ohe_col_size = 0
-df, X, y, transformer = read_analyze_transform("Output\\Sev_4.csv")
+df, X, y, transformer = read_analyze_transform("Output\\Sev_3.csv")
 X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=40)
 X_train = X_train.toarray()
 X = X.toarray()
-build_tree(X_train, y_train, y_test, 0.0)
+build_tree(X_train, y_train, 0.0)
+
+# remove exposure from feature set
+X_train = X_train[:, 1:]
+exposure_arr = X[:, :1]
+test_exposure = np.reshape(exposure_arr, np.shape(X)[0])
+X = X[:, 1:]
 
 model = joblib.load("SeverityRegressionTree.sav")
 y_pred = predict_tree(X, y, model)
-write_output(X, y,  y_pred)
+write_output(X, y, y_pred, test_exposure)
 
-# alpha_ = calculate_alpha(X_train, y_train, dt)
+# alpha_ = calculate_alpha(X_train, y_train, model)
 # dt_pruned = build_tree(X_train, y_train, alpha_)
-# y_pred_pruned = predict_tree(X_test, y_test, dt_pruned)
-# write_output(X_test, y_test,  y_pred_pruned)
+# y_pred_pruned = predict_tree(X, y, dt_pruned)
+# write_output(X, y,  y_pred_pruned, test_exposure)
 # plot_feature_importance()
 # plt.show()
 """
