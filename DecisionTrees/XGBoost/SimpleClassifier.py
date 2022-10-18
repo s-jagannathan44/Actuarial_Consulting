e54397fe6b5539217550db9f7e5274dc710f4f9c
s-jagannathan44 <85141510+s-jagannathan44@users.noreply.github.com>
2022-10-18 17:21:44 +0530

    Added the SelectFromModel functionality which allows for selecting features based on importance

--- DecisionTrees/XGBoost/SimpleClassifier.py
+++ DecisionTrees/XGBoost/SimpleClassifier.py
@@ -4,53 +4,53 @@ from collections import Counter
 import numpy as np
 import pandas as pd
 from sklearn.compose import ColumnTransformer
-from matplotlib import pyplot, pyplot as plt
-from numpy import where
-from sklearn.metrics import precision_recall_curve, accuracy_score, precision_score, recall_score, \
-     ConfusionMatrixDisplay
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import OrdinalEncoder, OneHotEncoder
-from xgboost import XGBClassifier
+from matplotlib import pyplot as plt
+from numpy import mean, sort
+from sklearn.feature_selection import SelectFromModel
+from sklearn.metrics import precision_recall_curve, precision_score, recall_score, \
+    ConfusionMatrixDisplay, f1_score
+from sklearn.model_selection import train_test_split, RepeatedStratifiedKFold, cross_val_score
+from sklearn.preprocessing import OrdinalEncoder, KBinsDiscretizer
+from xgboost import XGBClassifier, plot_importance
 
-ordinal_columns = ["GenderMainDriver"]
-ohe_columns = ["MaritalMainDriver", "Make", "Use", "PaymentMethod", "PaymentFrequency"]
+ordinal_columns = ["GenderMainDriver", "MaritalMainDriver", "Use", "PaymentMethod", "PaymentFrequency"]
 
 
-def make_plot_classification():
-    # summarize class distribution
-    counter = Counter(y)
-    print(counter)
-    # scatter plot of examples by class label
-    for label, _ in counter.items():
-        row_ix = where(y == label)[0]
-        pyplot.scatter(X[row_ix, 0], X[row_ix, 1], label=str(label))
-    pyplot.legend()
-    pyplot.show()
+def select_features():
+    threshold = sort(model.feature_importances_)
+    for thresh in threshold:
+        # select features using threshold
+        selection = SelectFromModel(model, threshold=thresh, prefit=True)
+        select_X_train = selection.transform(X_train)
+        # train model
+        selection_model = XGBClassifier(scale_pos_weight=Counter(y_train)[0] / Counter(y_train)[1])
+        selection_model.fit(select_X_train, y_train)
+        # eval model
+        select_X_test = selection.transform(X_test)
+        y_pred = selection_model.predict(select_X_test)
+        predictions_ = [round(value, ndigits=2) for value in y_pred]
+        f1_ = f1_score(y_test, predictions_)
+        print("Thresh=%.3f, n=%d, F1 score: %.2f%%" % (thresh, select_X_train.shape[1], f1_))
 
 
 def get_transformer():
     ordinal_tuple = ("ordinal", OrdinalEncoder(), ordinal_columns)
     transformer_ = ColumnTransformer(
-        [("passthrough_numeric", "passthrough", ["Exposure"]), ordinal_tuple,
-         ("onehot_categorical", OneHotEncoder(sparse=False), ohe_columns)],
-        remainder='drop')
+        [("binned_VehicleValue", KBinsDiscretizer(n_bins=10, encode='ordinal', strategy='uniform'), ["VehicleValue"]),
+         ordinal_tuple], remainder='drop')
     return transformer_
 
 
-# define dataset Mean ROC AUC: 0.53475
-# X, y = make_classification(n_samples=100000, n_features=7, n_redundant=0,
-#                           n_clusters_per_class=2, weights=[0.99], flip_y=0, random_state=7)
-
-df = pd.read_csv("Output\\creditcard.csv")
+df = pd.read_csv("Output\\WeightedPolicy.csv")
 
 transformer = get_transformer()
 
-X = df.drop('Class', axis=1)
-y = df["Class"]
-# X = transformer.fit_transform(X)
+X = df.drop('Actual', axis=1)
+y = df["Actual"]
+X = transformer.fit_transform(X)
 X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=25)
 # define model
-model = XGBClassifier(scale_pos_weight=Counter(y_train)[0]/Counter(y_train)[1])
+model = XGBClassifier(scale_pos_weight=Counter(y_train)[0] / Counter(y_train)[1])
 model.fit(X_train, y_train)
 predictions = model.predict(X_test)
 np.savetxt("Output\\output.csv", predictions, delimiter=",")
@@ -62,21 +62,23 @@ plt.ylabel('Precision')
 
 # calculate precision-recall AUC
 
-accuracy = accuracy_score(y_test, predictions)
+f1 = f1_score(y_test, predictions)
 precision = precision_score(y_test, predictions)
 recall = recall_score(y_test, predictions)
 
-print("Model Accuracy : ", accuracy)
+print("Model F1 : ", f1)
 print('Precision on testing set:', precision)
 print('Recall on testing set:', recall)
 
 # plotting the confusion-matrix
 ConfusionMatrixDisplay.from_predictions(y_test, predictions)
-plt.show()
 # define evaluation procedure
-# cv = RepeatedStratifiedKFold(n_splits=3, n_repeats=1, random_state=1)
+cv = RepeatedStratifiedKFold(n_splits=3, n_repeats=5, random_state=1)
 
 # evaluate model
-# scores = cross_val_score(model, X, y, scoring='roc_auc', cv=cv, n_jobs=-1, verbose=True)
+scores = cross_val_score(model, X, y, scoring='f1', cv=cv, n_jobs=-1, verbose=True)
 # summarize performance
-# print('Mean ROC AUC: %.5f' % mean(scores))
+print('Mean f1: %.5f' % mean(scores))
+# plot_importance(model)
+# plt.show()
+select_features()
