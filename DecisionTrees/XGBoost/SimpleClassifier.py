66734833ac85c1adc0505c2635ac1478ef01239a
s-jagannathan44 <85141510+s-jagannathan44@users.noreply.github.com>
2022-10-19 17:37:27 +0530

    Added code to write to file

--- DecisionTrees/XGBoost/SimpleClassifier.py
+++ DecisionTrees/XGBoost/SimpleClassifier.py
@@ -17,6 +17,19 @@ ordinal_columns = ["GenderMainDriver", "PaymentMethod"]
 ohe_columns = ["MaritalMainDriver", "Make", "Use", "PaymentFrequency"]
 
 
+def get_columns():
+    global ord_col_size, ohe_col_size
+    encoder = transformer.named_transformers_['onehot_categorical']
+    columns = encoder.get_feature_names_out()
+    ohe_col_size = columns.size
+    encoder = transformer.named_transformers_['ordinal']
+    ord_columns = encoder.get_feature_names_out()
+    columns = np.append(columns, ord_columns)
+    columns = np.append(columns, "VehicleValue")
+    ord_col_size = ord_columns.size
+    return columns
+
+
 def data_analysis(df=None):
     for c in df.columns:
         csv_file_name = "Output\\Columns\\" + c.replace("/", "_") + ".csv"
@@ -36,21 +49,38 @@ def select_features():
         selection_model.fit(select_X_train, y_train)
         # eval model
         select_X_test = selection.transform(X_test)
-        y_pred = selection_model.predict(select_X_test)
-        predictions_ = [round(value, ndigits=2) for value in y_pred]
-        f1_ = f1_score(y_test, predictions_)
+        y_pred_ = selection_model.predict(select_X_test)
+        predictions = [round(value, ndigits=2) for value in y_pred_]
+        f1_ = f1_score(y_test, predictions)
         print("Thresh=%.3f, n=%d, F1 score: %.2f" % (thresh, select_X_train.shape[1], f1_))
+
+
+def write_output():
+    ordinal_array = X_test[:, 1:ord_col_size+1].toarray()
+    passthrough = pd.DataFrame(X_test[:, :1].toarray(), columns=["VehicleValue"])
+    one_hot = transformer.named_transformers_['onehot_categorical'].inverse_transform(X_test[:, ord_col_size+1:])
+    ordinal = transformer.named_transformers_['ordinal'].inverse_transform(ordinal_array)
+    one_frame = pd.DataFrame(one_hot, columns=ohe_columns)
+    ordinal_frame = pd.DataFrame(ordinal, columns=ordinal_columns)
+
+    # axis 0 is vertical and axis 1 is horizontal
+    frame = pd.concat([passthrough, ordinal_frame], axis=1)
+    frame = pd.concat([frame, one_frame], axis=1)
+    frame["Actual"] = y_test.to_list()
+    frame['Predicted'] = y_pred
+    frame.to_csv("Output\\Output.csv")
 
 
 def get_transformer():
     ordinal_tuple = ("ordinal", OrdinalEncoder(), ordinal_columns)
-    ohe_tuple = ("ohe", OneHotEncoder(), ohe_columns)
+    ohe_tuple = ("onehot_categorical", OneHotEncoder(), ohe_columns)
     transformer_ = ColumnTransformer(
         [("passthrough_n", "passthrough", ["VehicleValue"]),
          ordinal_tuple, ohe_tuple], remainder='drop')
     return transformer_
 
 
+ord_col_size = ohe_col_size = 0
 df_ = pd.read_csv("Output\\WeightedPolicy.csv")
 transformer = get_transformer()
 
@@ -58,43 +88,43 @@ X = df_.drop('Actual', axis=1)
 y = df_["Actual"]
 
 X = transformer.fit_transform(X)
-sm = ADASYN()
-X, y = sm.fit_resample(X, y)
-
+sm = ADASYN(sampling_strategy="all")
+# X, y = sm.fit_resample(X, y)
 X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=25)
-
+X_train, y_train = sm.fit_resample(X_train, y_train)
 
 # define model
 model = XGBClassifier(scale_pos_weight=Counter(y_train)[0] / Counter(y_train)[1])
 model.fit(X_train, y_train)
 
-predictions = model.predict(X_test)
-np.savetxt("Output\\output.csv", predictions, delimiter=",")
-np.savetxt("Output\\y_test.csv", y_test, delimiter=",")
-precision, recall, thresholds = precision_recall_curve(y_test, predictions)
+y_pred = model.predict(X_test)
+
+precision, recall, thresholds = precision_recall_curve(y_test, y_pred)
 plt.plot(recall, precision, linestyle='--')
 plt.xlabel('Recall')
 plt.ylabel('Precision')
 
 # calculate precision-recall AUC
 
-f1 = f1_score(y_test, predictions)
-precision = precision_score(y_test, predictions)
-recall = recall_score(y_test, predictions)
+f1 = f1_score(y_test, y_pred)
+precision = precision_score(y_test, y_pred)
+recall = recall_score(y_test, y_pred)
 
 print("Model F1 : ", f1)
 print('Precision on testing set:', precision)
 print('Recall on testing set:', recall)
 
 # plotting the confusion-matrix
-ConfusionMatrixDisplay.from_predictions(y_test, predictions)
+ConfusionMatrixDisplay.from_predictions(y_test, y_pred)
 # define evaluation procedure
 cv = RepeatedStratifiedKFold(n_splits=3, n_repeats=5, random_state=1)
 
 # evaluate model
-scores = cross_val_score(model, X, y, scoring='f1', cv=cv, n_jobs=-1, verbose=True)
+scores = 0  # cross_val_score(model, X_test, y_test, scoring='f1', cv=cv, n_jobs=-1, verbose=True)
 # summarize performance
 print('Mean f1: %.5f' % mean(scores))
 # plot_importance(model)
 # select_features()
 plt.show()
+get_columns()
+write_output()
