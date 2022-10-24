26fb7b4c8a929aaaddf0df6438404f3cc25936bd
s-jagannathan44 <85141510+s-jagannathan44@users.noreply.github.com>
2022-10-24 21:39:59 +0530

    UK Data 41 % accuracy 67.5 % recall

--- DecisionTrees/XGBoost/SimpleClassifier.py
+++ DecisionTrees/XGBoost/SimpleClassifier.py
@@ -10,14 +10,11 @@ from Modules import Utilities as Ut
 from xgboost import XGBClassifier
 
 # Make and Payment_frequency to go under One hot encoding
-passthrough_list = ["AnalysisPeriod", "NumberOfDrivers", "VoluntaryExcess", "NumberOfPastClaims",
-                    "NumberOfPastConvictions", "ClaimLastYr"]
-ordinal_list = ["GenderMainDriver", "GenderYoungestDriver", "MaritalMainDriver",
-                "Use", "PaymentMethod", "BonusMalusProtection",
-                "GenderYoungestAdditionalDriver", "VehFuel1"]
-to_bin_list = [['AgeMainDriver', 4, 'uniform'], ['AgeYoungestDriver', 4, 'uniform'],
-               ['AgeYoungestAdditionalDriver', 2, 'uniform'], ['VehicleAge', 2, 'uniform'],
-               ['VehicleValue', 10, 'uniform'], ['VehicleMileage', 4, 'uniform'],
+passthrough_list = ["AnalysisPeriod"]
+ordinal_list = ["GenderMainDriver", "GenderYoungestDriver",
+                "Use", "PaymentMethod", "BonusMalusProtection"]
+to_bin_list = [['AgeMainDriver', 4, 'uniform'],
+               ['VehicleAge', 2, 'uniform'],
                ['BonusMalusYears', 4, 'quantile'], ['PolicyTenure', 3, 'quantile']]
 
 
@@ -37,8 +34,8 @@ def select_features():
         select_X_train = selection.transform(X_train)
         # train model
         selection_model = XGBClassifier(objective='binary:logistic', seed=42, base_score=0.05,
-                                        max_depth=3, n_estimators=200, colsample_bytree=0.8,
-                                        reg_lambda=10, scale_pos_weight=10)
+                                        learning_rate=0.01, max_depth=7, n_estimators=500, colsample_bytree=0.7,
+                                        gamma=3, reg_alpha=10, reg_lambda=0.5, scale_pos_weight=35)
         selection_model.fit(select_X_train, y_train)
         # eval model
         select_X_test = selection.transform(X_test)
@@ -51,7 +48,7 @@ def select_features():
 def gridsearch(rgr):
     param_grid = \
         {
-
+            'learning_rate': [0.01, 0.05, 0.3, 0.5]
         }
     model = GridSearchCV(rgr, param_grid, cv=10, scoring="f1", refit=True, verbose=3, n_jobs=-1)
     model.fit(X_train, y_train)
@@ -62,26 +59,20 @@ def gridsearch(rgr):
 # -------------------- CODE STARTS HERE ---------------------------------------
 ord_col_size = ohe_col_size = 0
 df_ = pd.read_csv("Output\\Policies.csv")
-df_ = df_[df_["MaritalMainDriver"] != "Partnership"]
 df_ = df_[df_["Use"] != "Business Use - Class 1"]
 df_ = df_[df_["PaymentMethod"] != "Other"]
 
 X = df_.drop('Claim Count', axis=1)
 y = df_["Claim Count"]
-X = Ut.impute_missing_values(X, "AgeYoungestAdditionalDriver")
-X = Ut.impute_missing_values(X, "GenderYoungestAdditionalDriver")
-
 transformer = Ut.transform(passthrough_list, to_bin_list, ordinal_list)
-
-
 X = transformer.fit_transform(X)
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, stratify=y, random_state=25)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, stratify=y, random_state=25)
 
 # define model
 estimator_ = XGBClassifier(objective='binary:logistic', seed=42, base_score=0.05,
-                           max_depth=3, n_estimators=200, colsample_bytree=0.8,
-                           reg_lambda=10, scale_pos_weight=10)
+                           learning_rate=0.01, max_depth=7, n_estimators=500, colsample_bytree=0.7,
+                           gamma=3, reg_alpha=10, reg_lambda=10, scale_pos_weight=35)
 estimator_.fit(X_train, y_train)
 # estimator_ = gridsearch(estimator_)
 y_pred = estimator_.predict(X_test)
@@ -106,14 +97,11 @@ scores = 0  # cross_val_score(estimator_, X_test, y_test, scoring='f1', cv=cv, n
 print('Mean f1: %.5f' % mean(scores))
 # plot_importance(estimator_)
 
-(pd.Series(estimator_.feature_importances_, index=["AnalysisPeriod", "NumberOfDrivers", "VoluntaryExcess",
-                                                   "NumberOfPastClaims", "NumberOfPastConvictions", "ClaimLastYr",
-                                                   "AgeMainDriver", 'AgeYoungestDriver', 'AgeYoungestAdditionalDriver',
-                                                   'VehicleAge', 'VehicleValue', 'VehicleMileage', 'BonusMalusYears',
+(pd.Series(estimator_.feature_importances_, index=["AnalysisPeriod", "AgeMainDriver",
+                                                   'VehicleAge', 'BonusMalusYears',
                                                    'PolicyTenure', "GenderMainDriver", "GenderYoungestDriver",
-                                                   "MaritalMainDriver", "Use", "PaymentMethod", "BonusMalusProtection",
-                                                   "GenderYoungestAdditionalDriver", "VehFuel1"])
-   .nlargest(20)
-   .plot(kind='barh'))
+                                                   "Use", "PaymentMethod", "BonusMalusProtection"])
+ .nlargest(10)
+ .plot(kind='barh'))
 select_features()
 plt.show()
