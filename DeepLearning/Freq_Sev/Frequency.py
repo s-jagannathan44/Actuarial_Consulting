e3bb64b5909901b8b4541e7b8dfa22180b46ff0d
s-jagannathan44 <85141510+s-jagannathan44@users.noreply.github.com>
2022-11-23 13:09:28 +0530

    Neural Network classification model updated

--- DeepLearning/Freq_Sev/Frequency.py
+++ DeepLearning/Freq_Sev/Frequency.py
@@ -1,88 +1,92 @@
-import numpy as np
 import pandas as pd
-from keras.metrics import AUC
-from matplotlib import pyplot as plt
-from sklearn.compose import ColumnTransformer
+from keras.callbacks import ModelCheckpoint, EarlyStopping
+from keras.optimizers import SGD
 from sklearn.model_selection import train_test_split
+from Modules import Utilities as Ut
 from keras.models import Sequential
 from keras.layers import Dense
-from sklearn.preprocessing import OrdinalEncoder
-from keras.callbacks import ModelCheckpoint, EarlyStopping
-from Modules import Utilities as Ut
 
 
-def plot_curve(epoch, his, list_of_metrics):
-    """Plot a curve of one or more classification metrics vs. epoch."""
-    # list_of_metrics should be one of the names shown in:
-    # https://www.tensorflow.org/tutorials/structured_data/imbalanced_data#define_the_model_and_metrics
-
-    plt.figure()
-    plt.xlabel("Epoch")
-    plt.ylabel("Value")
-    for m in list_of_metrics:
-        print(m)
-        x = his['auc']
-        plt.plot(epoch[1:], x[1:], label='AUC')
-    plt.legend()
-    plt.show()
+# Make and Payment_frequency to go under One hot encoding
+passthrough_list = ["AnalysisPeriod"]
+ordinal_list = ["GenderMainDriver", "GenderYoungestDriver",
+                "Use", "PaymentMethod", "BonusMalusProtection"]
+to_bin_list = [['AgeMainDriver', 4, 'uniform'],
+               ['VehicleAge', 2, 'uniform'],
+               ['BonusMalusYears', 4, 'quantile'], ['PolicyTenure', 3, 'quantile']]
 
 
-def create_model():
-    # Define the model
-    model = Sequential([
+def get_columns():
+    columns = {}
+    for encoder in transformer.named_transformers_:
+        if type(transformer.named_transformers_[encoder]) != str:
+            item = [(encoder, transformer.named_transformers_[encoder].get_feature_names_out().size)]
+            columns.update(item)
+    return columns
+
+
+def write_output(X_value, actual_val, pred_val):
+    frame_list = []
+    res = []
+    length = 0
+    index = 0
+    for item in column_dict:
+        encoder = transformer.named_transformers_[item]
+        size = column_dict[item]
+        length = length + size
+        if length == size:
+            col = encoder.inverse_transform(X_value[:, :length])
+        else:
+            col = encoder.inverse_transform(X_value[:, index:length])
+        frame = pd.DataFrame(col, columns=encoder.get_feature_names_out())
+        frame_list.append(frame)
+        index = length
+
+    for frame in frame_list:
+        res.append(frame)
+
+    # axis 0 is rows and axis 1 is columns
+    frame = pd.concat(res, axis=1)
+    frame["Actual"] = actual_val.to_list()
+    frame['Predicted_Proba'] = pred_val
+    frame.to_csv("Output\\Output.csv")
+
+
+def data_analysis(df=None):
+    for c in df.columns:
+        csv_file_name = "Output\\Columns\\" + c.replace("/", "_") + ".csv"
+        insight = df[c].value_counts()
+        insight.to_csv(csv_file_name)
+    df.describe(percentiles=[0.25, 0.5, 0.75, 0.85, 0.9, 0.98, 1]).to_csv("Output\\Columns\\desc.csv")
+
+
+# -------------------- CODE STARTS HERE ---------------------------------------
+ord_col_size = ohe_col_size = 0
+df_ = pd.read_csv("Output\\Policies.csv")
+df_ = df_[df_["Use"] != "Business Use - Class 1"]
+df_ = df_[df_["PaymentMethod"] != "Other"]
+
+X = df_.drop('Claim Count', axis=1)
+y = df_["Claim Count"]
+transformer = Ut.transform(passthrough_list, to_bin_list, ordinal_list)
+X = transformer.fit_transform(X)
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, stratify=y, random_state=25)
+
+# define model
+estimator_ = Sequential([
         Dense(5, activation='relu', input_shape=(X_train.shape[1],)),
         Dense(10, activation='relu'),
         Dense(5, activation='relu'),
         Dense(1, activation="sigmoid"),
     ])
-    model.compile(optimizer='adam',
-                  loss='binary_crossentropy',
-                  metrics=AUC(name='auc'))
-    checkpoint = ModelCheckpoint(filepath="Output\\Checkpoint.h5", monitor="auc", verbose=1,
-                                 save_best_only=True, mode="max")
-    early_stopping = EarlyStopping(monitor="auc", min_delta=0.001, patience=5)
-    history = model.fit(X_train, y_train, epochs=30, batch_size=500,
-                        callbacks=[checkpoint, early_stopping])
-    # The list of epochs is stored separately from the rest of history.
-    epochs = history.epoch
-    # Isolate the classification metric for each epoch.
-    hist = pd.DataFrame(history.history)
-    metric_list = ['auc']
-    plot_curve(epochs, hist, metric_list)
+checkpoint = ModelCheckpoint(filepath="Output\\Checkpoint.h5", monitor="poisson", verbose=1,
+                             save_best_only=True, mode="min")
+early_stopping = EarlyStopping(monitor="poisson", min_delta=0.00001, patience=5)
+estimator_.compile(optimizer=SGD(learning_rate=0.01, momentum=0.1), loss="poisson", metrics="poisson")
+estimator_.fit(X, y, epochs=1000, batch_size=1000, callbacks=[checkpoint, early_stopping])
 
-    return model
-
-
-df = pd.read_csv("Output\\Input_5.csv")
-X = df.drop("Claim", axis=1)
-y = df["Claim"]
-
-transformer = ColumnTransformer(
-    [
-        (
-            "onehot_categorical",
-            OrdinalEncoder(),
-            ["GenderMainDriver", "MaritalMainDriver",  "DrivingRestriction", "Make", "VehFuel1"],
-        ),
-    ],
-    remainder='drop'
-)
-
-X = transformer.fit_transform(X)
-
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.30, random_state=40)
-
-y_pred_ = create_model().predict(X_test)
-
-
-X_test = transformer.named_transformers_['onehot_categorical'].inverse_transform(X_test)
-frame = pd.DataFrame(X_test, columns=["GenderMainDriver", "MaritalMainDriver",  "DrivingRestriction",
-                                      "Make", "VehFuel1"])
-
-frame.to_csv("Output\\X_test.csv")
-np.savetxt("Output\\y_test.csv", y_test, delimiter=",")
-np.savetxt("Output\\y_pred.csv", y_pred_, delimiter=",")
-
-columns_ = ["GenderMainDriver", "MaritalMainDriver",  "DrivingRestriction", "Make", "VehFuel1"]
-levels_ = [0, 1, 2, 3, 4]
-Ut.plot_scatter(columns_, levels_)
+# estimator_ = gridsearch(estimator_)
+y_pred = estimator_.predict(X_test)
+column_dict = get_columns()
+write_output(X_test, y_test, y_pred)
