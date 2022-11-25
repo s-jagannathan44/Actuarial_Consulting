6cc558b4ead22085a07afc3a2c3fad8065a7a4be
s-jagannathan44 <85141510+s-jagannathan44@users.noreply.github.com>
2022-11-25 17:22:27 +0530

    French Motor  Model updated to use Predict_proba

--- DecisionTrees/XGBoost/French.py
+++ DecisionTrees/XGBoost/French.py
@@ -1,119 +1,78 @@
 import math
-from matplotlib import pyplot
-from numpy import sort
-from sklearn.feature_selection import SelectFromModel
-from sklearn.metrics import mean_absolute_error
-from sklearn.model_selection import GroupShuffleSplit, GridSearchCV
-from xgboost import XGBRegressor, plot_importance
+import numpy as np
+from xgboost import XGBClassifier
 import Modules.Utilities as Ut
+from sklearn.model_selection import GroupShuffleSplit
 import pandas as pd
 
 
-def describe():
-    df = pd.read_csv("Output\\FMTPL.csv")
-    temp = df.drop(['IDpol', 'Exposure', 'ClaimNb', 'Claim'], axis=1)
-    df_freq = temp.iloc[temp.drop_duplicates().index]
-    df_freq = df_freq.reset_index(drop=True)
-    df_freq['GroupID'] = df_freq.index + 1
-    df_freq.to_csv("Output\\Unique.csv")
-    df_freq = pd.merge(df, df_freq, how='left')
-    df_freq['GroupID'] = df_freq['GroupID'].fillna(method='ffill')
-    print(df_freq['GroupID'].max())
-    df_freq.to_csv("Output\\Grouped.csv")
-
-    df_freq = df_freq[df_freq["Claim"] > 0]
-    df_freq = df_freq.drop('IDpol', axis=1)
-    for c in df_freq.columns:
-        csv_file_name = "Output\\Columns\\" + c + ".csv"
-        insight = df[c].value_counts()
-        insight.to_csv(csv_file_name)
-    df_freq.describe(percentiles=[0.25, 0.5, 0.75, 0.85, 0.9, 0.98, 1]).to_csv("Output\\Columns\\desc.csv")
-    df_freq.corr().to_csv("Output\\Columns\\corr.csv")
-
-
-def select_features():
-    thresholds = sort(estimator_.feature_importances_)
-    for thresh in thresholds:
-        # select features using threshold
-        selection = SelectFromModel(estimator_, threshold=thresh, prefit=True)
-        select_X_train = selection.transform(X_train)
-        # train model
-        selection_model = XGBRegressor(objective='reg:gamma', seed=42, eval_metric='mae', max_depth=6,
-                                       gamma=0.8, learning_rate=0.15, n_estimators=300, colsample_bytree=0.9)
-        selection_model.fit(select_X_train, y_train)
-        # eval model
-        select_X_test = selection.transform(X_test)
-        predictions = selection_model.predict(select_X_test)
-        print("Thresh=%.3f, n=%d, Accuracy: " % (thresh, select_X_train.shape[1]))
-        write_output(y_test, predictions)
-
-
-def read_transform():
-    df = pd.read_csv("Output\\Grouped.csv")
-    df['VehAge'] = df['VehAge'].apply(lambda x: 20 if x > 20 else x)
-    df['DrivAge'] = df['DrivAge'].apply(lambda x: 90 if x > 90 else x)
-    df['BonusMalus'] = df['BonusMalus'].apply(lambda x: 150 if x > 150 else int(x))
-    df['Density'] = df['Density'].apply(lambda x: round(math.log(x), 2))
-    df['Exposure'] = df['Exposure'].apply(lambda x: 1 if x > 1 else x)
-    df_freq_ml = Ut.motor_third_party_transform(df)
-    df_freq_ml = df_freq_ml[df_freq_ml["Claim"] > 0]
-
-    df_freq_ml["Target"] = df_freq_ml['Claim'] / df_freq_ml['ClaimNb']
-    splitter = GroupShuffleSplit(test_size=0.2, n_splits=2, random_state=999)
-    split = splitter.split(df_freq_ml, groups=df_freq_ml['GroupID'])
-    train_ind, test_ind = next(split)
-    df_freq_ml = df_freq_ml.drop(['ClaimNb', 'Exposure', 'GroupID', 'Claim'], axis=1)
+def data_verification():
+    # Step 6 Check if the proportion of train set to test set is indeed 80:20
+    global train, test
     train = df_freq_ml.iloc[train_ind]
     test = df_freq_ml.iloc[test_ind]
-    df_freq_ml.to_csv("Output\\normalised.csv")
-    return train.drop("Target", axis=1), train["Target"], test.drop("Target", axis=1), test["Target"]
+    print("train %age ", len(train) / len(df_freq_ml))
+    print("test %age ", len(test) / len(df_freq_ml))
+    # Step 7 check if the average claims frequencies are similar between train set and test set.
+    # They should be very similar, otherwise that means group shuffle has not been done appropriately.
+    print("training Claim frequency", train['ClaimNb'].sum() / train['Exposure'].sum())
+    print("test Claim frequency", test['ClaimNb'].sum() / test['Exposure'].sum())
+    # Step 8 Check the sum of Exposure and count of ClaimNb in the train and test sets.
+    print(train.groupby(by=['ClaimNb']).agg({'Exposure': ['sum'], 'ClaimNb': ['count']}))
+    print('\n-------------------------\n')
+    print(test.groupby(by=['ClaimNb']).agg({'Exposure': ['sum'], 'ClaimNb': ['count']}))
 
 
-def predict(X_val, y_val, estimator):
-    # We will predict the output  here
-    y_pred_ = estimator.predict(X_val)
+# Step 1 read File
+df_freq = pd.read_csv('Output\\Grouped.csv')
 
-    mean_baseline = y_val.mean()
-    y_pred_base = [mean_baseline] * len(y_val)
-    mae_base = mean_absolute_error(y_val, y_pred_base)
-    print(f'Mean Baseline: {mean_baseline:.1f} ')
-    print(f'Baseline mean absolute error: {mae_base}')
+'''
+Step3 We will also simplify the data for our model. In particular, we will adjust the following columns:
+VehAge: cap at 20 years
+DrivAge: cap at 90 years old
+BonusMalus: cap at 150, round to nearest integer
+Density: apply log
+Exposure: cap at 1 year
+'''
+df_freq['VehAge'] = df_freq['VehAge'].apply(lambda x: 20 if x > 20 else x)
+df_freq['DrivAge'] = df_freq['DrivAge'].apply(lambda x: 90 if x > 90 else x)
+df_freq['BonusMalus'] = df_freq['BonusMalus'].apply(lambda x: 150 if x > 150 else int(x))
+df_freq['Density'] = df_freq['Density'].apply(lambda x: round(math.log(x), 2))
+df_freq['Exposure'] = df_freq['Exposure'].apply(lambda x: 1. if x > 1 else x)
+df_freq['Exposure'] = df_freq['Exposure'].apply(lambda x: 1. if x > 1 else x)
+df_freq['ClaimNb'] = df_freq['ClaimNb'].apply(lambda x: 1. if x > 0 else x)
+# Step 4 Normalize the data.
+# we use One hot  encoding for the feature components VehBrand and Region
+# We use the MinMaxScaler for Area (after transforming {A,...,F} ↦ {1,...,6})
+# VehPower , VehAge , DrivAge , BonusMalus, Density
+# VehGas we transform to ±1/2 and the volume Exposure 0-1 we keep untransformed
 
-    print('predicted mean', y_pred_.mean())
-    mae_model = mean_absolute_error(y_val, y_pred_)
-    print(f'Model mean absolute error: {mae_model}')
-    return y_pred_
+df_freq_ml = Ut.motor_third_party_transform(df_freq)
+df_freq_ml["Target"] = df_freq_ml['ClaimNb']
 
+# Step 5 create  a copy and split  using GroupShuffleSplit instead of train_test_split
+# df_freq_ml = deepcopy(df_freq)
+splitter = GroupShuffleSplit(test_size=0.2, n_splits=2, random_state=999)
+split = splitter.split(df_freq_ml, groups=df_freq_ml['GroupID'])
+train_ind, test_ind = next(split)
+data_verification()
 
-def write_output(actual_val, pred_val):
-    frame = pd.DataFrame(columns=["Actual", "Predicted", "Error", '%ageError', 'Below5%'])
-    frame["Actual"] = actual_val.to_list()
-    frame['Predicted'] = pred_val
-    frame["Error"] = abs(frame["Actual"] - frame["Predicted"])
-    frame["%ageError"] = frame["Error"] / frame["Actual"]
-    frame["Below5%"] = round(frame[frame["%ageError"] < 0.05].shape[0] / frame.shape[0], ndigits=2)
-    print(round((frame[frame["%ageError"] < 0.05].shape[0] / frame.shape[0]) * 100, ndigits=2))
-    # frame.to_csv("Output\\Output.csv")
+df_freq_ml = df_freq_ml.drop(['ClaimNb', 'Exposure', 'GroupID', 'Claim'], axis=1)
+# df_freq_ml = df_freq_ml.drop(['BonusMalus', 'Density', 'Area'], axis=1)
 
+train = df_freq_ml.iloc[train_ind]
+test = df_freq_ml.iloc[test_ind]
 
-def gridsearch(rgr):
-    param_grid = \
-        {
-            'gamma': [0.8]
-        }
-    model = GridSearchCV(rgr, param_grid, cv=10, scoring="neg_mean_absolute_error", refit=True, verbose=3, n_jobs=-1)
-    model.fit(X_train, y_train)
-    print(model.best_params_)
-    return model.best_estimator_
+# print(df_freq_ml.describe())
+# df_freq_ml = df_freq_ml.drop(['ClaimNb', 'Exposure', 'GroupID', 'VehGas'], axis=1)
+df_freq_ml.to_csv("Output\\normalised.csv")
 
+X_train, y_train = Ut.fetch_xy(train, 7)
+X_test, y_test = Ut.fetch_xy(test, 7)
 
-X_train, y_train, X_test, y_test = read_transform()
-estimator_ = XGBRegressor(objective='reg:gamma', seed=42, eval_metric='mae', max_depth=6,
-                          gamma=0.8, learning_rate=0.15, n_estimators=300, colsample_bytree=0.9)
+estimator_ = XGBClassifier()
 estimator_.fit(X_train, y_train)
-# estimator_ = gridsearch(estimator_)
-plot_importance(estimator_)
-y_pred = predict(X_test, y_test, estimator_)
-write_output(y_test, y_pred)
-# pyplot.show()
-select_features()
+y_pred_proba = estimator_.predict_proba(X_test)
+np.savetxt("Output\\y_pred.csv", y_pred_proba, delimiter=",")
+np.savetxt("Output\\X_test.csv", X_test, delimiter=",")
+np.savetxt("Output\\y_test.csv", y_test, delimiter=",")
--- DeepLearning/Classification/InsuranceClassifcation.py
+++ DeepLearning/Classification/InsuranceClassifcation.py
@@ -1,8 +1,11 @@
 import math
+import numpy as np
+from keras.optimizers import SGD
+from tensorflow import keras
 import Modules.Utilities as Ut
 from sklearn.model_selection import GroupShuffleSplit
 import pandas as pd
-from keras.metrics import MeanSquaredError, Poisson
+from keras.metrics import Poisson
 
 
 def data_verification():
@@ -23,24 +26,7 @@ def data_verification():
 
 
 # Step 1 read File
-freq = pd.read_csv('Output\\Input.csv')
-print(freq.describe())
-print(freq.corr())
-
-# Step 2 Add group id by clubbing rows which belong to the same policy as one group.
-# This is done by finding all rows which have same values for below columns
-# Area	VehPower	VehAge	Driver Age	BonusMalus	VehBrand	VehGas	Density	Region
-# This is achieved by dropping all columns other than above , finding duplicate rows
-# and then and assigning them the same group id
-# Once the group Ids have been found the new dataframe is merged back into the old data frame using
-# a left outer join and the missing group ids are filled in
-
-df_freq = freq.iloc[freq.drop(['IDpol', 'Exposure', 'ClaimNb'], axis=1).drop_duplicates().index]
-df_freq = df_freq.reset_index(drop=True)
-df_freq['GroupID'] = df_freq.index + 1
-df_freq = pd.merge(freq, df_freq, how='left')
-df_freq['GroupID'] = df_freq['GroupID'].fillna(method='ffill')
-print(df_freq['GroupID'].max())
+df_freq = pd.read_csv('Output\\Grouped.csv')
 
 '''
 Step3 We will also simplify the data for our model. In particular, we will adjust the following columns:
@@ -55,7 +41,8 @@ df_freq['DrivAge'] = df_freq['DrivAge'].apply(lambda x: 90 if x > 90 else x)
 df_freq['BonusMalus'] = df_freq['BonusMalus'].apply(lambda x: 150 if x > 150 else int(x))
 df_freq['Density'] = df_freq['Density'].apply(lambda x: round(math.log(x), 2))
 df_freq['Exposure'] = df_freq['Exposure'].apply(lambda x: 1. if x > 1 else x)
-
+df_freq['Exposure'] = df_freq['Exposure'].apply(lambda x: 1. if x > 1 else x)
+df_freq['ClaimNb'] = df_freq['ClaimNb'].apply(lambda x: 1. if x > 0 else x)
 # Step 4 Normalize the data.
 # we use One hot  encoding for the feature components VehBrand and Region
 # We use the MinMaxScaler for Area (after transforming {A,...,F} ↦ {1,...,6})
@@ -63,7 +50,7 @@ df_freq['Exposure'] = df_freq['Exposure'].apply(lambda x: 1. if x > 1 else x)
 # VehGas we transform to ±1/2 and the volume Exposure 0-1 we keep untransformed
 
 df_freq_ml = Ut.motor_third_party_transform(df_freq)
-df_freq_ml["Target"] = df_freq_ml['ClaimNb'] / df_freq_ml['Exposure']
+df_freq_ml["Target"] = df_freq_ml['ClaimNb']
 
 # Step 5 create  a copy and split  using GroupShuffleSplit instead of train_test_split
 # df_freq_ml = deepcopy(df_freq)
@@ -72,7 +59,7 @@ split = splitter.split(df_freq_ml, groups=df_freq_ml['GroupID'])
 train_ind, test_ind = next(split)
 data_verification()
 
-df_freq_ml = df_freq_ml.drop(['ClaimNb', 'Exposure', 'GroupID'], axis=1)
+df_freq_ml = df_freq_ml.drop(['ClaimNb', 'Exposure', 'GroupID', 'Claim'], axis=1)
 # df_freq_ml = df_freq_ml.drop(['BonusMalus', 'Density', 'Area'], axis=1)
 
 train = df_freq_ml.iloc[train_ind]
@@ -85,8 +72,12 @@ df_freq_ml.to_csv("Output\\normalised.csv")
 X_train, y_train = Ut.fetch_xy(train, 7)
 X_test, y_test = Ut.fetch_xy(test, 7)
 
-metrics = [
-    Poisson(name="mean_absolute_percentage_error")]
+metrics = [Poisson()]
 
-# model = Ut.create_model(X_train.shape[1], X_train, y_train, metrics)
-# Ut.load_predict("Output\\Checkpoint.h5", X_test)
+model = Ut.create_model(X_train.shape[1], X_train, y_train, metrics)
+my_model = keras.models.load_model("Output\\Checkpoint.h5")
+my_model.compile(optimizer=SGD(learning_rate=0.01, momentum=0.1), loss="poisson", metrics=metrics)
+
+Ut.load_predict(my_model, X_test)
+np.savetxt("Output\\X_test.csv", X_test, delimiter=",")
+np.savetxt("Output\\y_test.csv", y_test, delimiter=",")
