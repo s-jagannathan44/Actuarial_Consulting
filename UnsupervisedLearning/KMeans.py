841fc8e03217b8a1316119d1b9907e132f37a9ba
s-jagannathan44 <85141510+s-jagannathan44@users.noreply.github.com>
2022-12-27 15:03:12 +0530

    Excel files for telematics where we cluster using city/Highway traffic and by deviced Id

--- UnsupervisedLearning/KMeans.py
+++ UnsupervisedLearning/KMeans.py
@@ -4,6 +4,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn.cluster import KMeans as Km
+from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 
 
@@ -35,21 +36,16 @@ def read_transform():
     df.to_csv("Output\\interim.csv")
 
 
-df_ = pd.read_csv("Output\\v5.csv")
-df_ = df_[df_["deviceID"] != 14]
-# Plot the data
-plt.figure(figsize=(6, 6))
-plt.scatter(df_.iloc[:, 4], df_.iloc[:, 5])
-plt.xlabel('Speed')
-plt.ylabel('Acceleration')
-plt.ylim(-10, 10)
-plt.xlim(0, 60)
-plt.title('Visualization of raw data')
+# -------------------- CODE STARTS HERE ---------------------------------------
 
+df_ = pd.read_csv("Output\\HighwayTraffic.csv")
+# df_ = df_[df_["deviceID"] != 14]
+# train, test = train_test_split(df_, test_size=0.5, random_state=42)
+# train.to_csv("train.csv")
+# train.to_csv("test.csv")
 
 # Standardize the data
 X_std = StandardScaler().fit_transform(df_.drop(["timeStamp", "Interval"], axis=1))
-# X_std = df_.drop("timeStamp", axis=1)
 # Run local implementation of kmeans
 km = Km(n_clusters=4, max_iter=100)
 km.fit(X_std)
@@ -58,18 +54,6 @@ df_["Cluster"] = km.labels_
 df_.to_csv("Output\\clustered.csv")
 
 # Plot the clustered data
-fig, ax = plt.subplots(figsize=(6, 6))
-plt.scatter(X_std[km.labels_ == 0, 0], X_std[km.labels_ == 0, 1],
-            c='green', label='cluster 1')
-plt.scatter(X_std[km.labels_ == 1, 0], X_std[km.labels_ == 1, 1],
-            c='blue', label='cluster 2')
-plt.scatter(centroids[:, 0], centroids[:, 1], marker='*', s=300,
-            c='r', label='centroid')
-plt.legend()
-plt.xlabel('Speed')
-plt.ylabel('Acceleration')
-plt.title('Visualization of clustered data', fontweight='bold')
-ax.set_aspect('equal')
 
 # Run the Kmeans algorithm and get the index of data points clusters
 sse = []
