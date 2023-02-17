7d04b7a5c33d1df13d3e935329f8138f240d4382
s-jagannathan44 <85141510+s-jagannathan44@users.noreply.github.com>
2023-02-17 17:19:22 +0530

    Implemented PCA Pipline with scoring which shows that we should go back normal clustering as dimensionality reduction provides no benefits when columns are few

--- UnsupervisedLearning/KMeans.py
+++ UnsupervisedLearning/KMeans.py
@@ -1,10 +1,39 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 from kneed import KneeLocator
 from sklearn.cluster import KMeans as Km
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.metrics import silhouette_score
 from sklearn.decomposition import PCA
 from sklearn.pipeline import Pipeline
+
+
+def score_PCA():
+    for n in range(1, 8):
+        # This set the number of components for pca,
+        # but leaves other steps unchanged
+        pipe["preprocessor"]["pca"].n_components = n
+        pipe.fit(df_)
+
+        silhouette_coef = silhouette_score(
+            pipe["preprocessor"].transform(df_),
+            pipe["clusterer"]["kmeans"].labels_,
+        )
+        # Add metrics to their lists
+        sil_scores.append(silhouette_coef)
+    plt.style.use("fivethirtyeight")
+    plt.figure(figsize=(6, 6))
+    plt.plot(
+        range(1, 8),
+        sil_scores,
+        c="#008fd5",
+        label="Silhouette Coefficient",
+    )
+    plt.xlabel("n_components")
+    plt.legend()
+    plt.title("Clustering Performance as a Function of n_components")
+    plt.tight_layout()
+    plt.show()
 
 
 def elbow_method(X_std):
@@ -14,6 +43,11 @@ def elbow_method(X_std):
         km.fit(X_std)
         sse.append(km.inertia_)
     # Plot sse against k
+    plt.figure(figsize=(6, 6))
+    plt.plot(list_k, sse, '-o')
+    plt.xlabel(r'Number of clusters $k$')
+    plt.ylabel('Sum of squared distance')
+    plt.show()
     return KneeLocator(range(2, 10), sse, curve="convex", direction="decreasing").elbow
 
 
@@ -21,19 +55,20 @@ def elbow_method(X_std):
 sse = []
 sil_scores = []
 davies_score = []
-n_clusters = 5
 df_ = pd.read_csv("Output\\FinalData.csv")
+n_clusters = elbow_method(MinMaxScaler().fit_transform(df_))
 # Create Pipelines
-preprocessor = Pipeline([("scaler", MinMaxScaler()), ("pca", PCA(n_components=4, random_state=42)), ])
+preprocessor = Pipeline([("scaler", MinMaxScaler()), ("pca", PCA(n_components=1, random_state=42)), ])
 clusterer = Pipeline([(
     "kmeans",
     Km(n_clusters=n_clusters, init="k-means++", n_init=50, max_iter=500, random_state=42, ),
 )])
 pipe = Pipeline([("preprocessor", preprocessor), ("clusterer", clusterer)])
-
+score_PCA()
 pipe.fit(df_)
 preprocessed_data = pipe["preprocessor"].transform(df_)
-n_clusters = elbow_method(preprocessed_data)
 print(n_clusters)
 predicted_labels = pipe["clusterer"]["kmeans"].labels_
+df_["Cluster"] = predicted_labels
+df_.to_csv("Output\\clustered.csv")
 print(silhouette_score(preprocessed_data, predicted_labels))
