15ef8d78df89f0ef641ae8988c1d956d252e2eb2
s-jagannathan44 <85141510+s-jagannathan44@users.noreply.github.com>
2023-03-06 07:03:04 +0530

    Changed Death file, so it now has 11 columns and 3 lakh rows

--- Project/Death.py
+++ Project/Death.py
@@ -120,7 +120,8 @@ def combine_claim_files():
 
 def rollup_policies(freq):
     col_list = ["UY New", "LT_ANNUAL Flag", "CC_desc", "Body Type", "Vehicle Make", "V AGE BAND",
-                "Registration States", "Zone"]
+                "Registration States", "Zone", "Channel", "Cluster", "Vehicle Registration Region"]
+
     full_list = col_list + ['Gross Cost'] + ['Exposure']
 
     freq = freq[full_list]
