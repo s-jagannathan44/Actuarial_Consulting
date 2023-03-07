35b07be46dde826270d70bcd340596ca702e757b
s-jagannathan44 <85141510+s-jagannathan44@users.noreply.github.com>
2023-03-07 15:12:15 +0530

    GLM Gamma / Tweedie Model for Injury

--- Project/Tweedie_GLM.py
+++ Project/Tweedie_GLM.py
@@ -1,24 +1,24 @@
-# import joblib
-import numpy as np
 import pandas as pd
-from sklearn.model_selection import train_test_split
+# from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import OneHotEncoder
-from sklearn.preprocessing import KBinsDiscretizer
+# from sklearn.preprocessing import KBinsDiscretizer
 from sklearn.compose import ColumnTransformer
 from sklearn.pipeline import Pipeline
-from sklearn.linear_model import TweedieRegressor
+# from sklearn.linear_model import TweedieRegressor
+from sklearn.linear_model import GammaRegressor
 
 
 def build_model():
     global df_train, df_test
     linear_model_preprocessor = ColumnTransformer(
         [
-            ("binned_numeric", KBinsDiscretizer(n_bins=10), ["VehicleValue"]),
-            ("passthrough_numeric", "passthrough", ["Exposure"]),
+            # ("binned_numeric", KBinsDiscretizer(n_bins=3, strategy='uniform'), ["V AGE BAND"]),
+            ("passthrough_numeric", "passthrough", ["CC_desc"]),
+            ("passthrough_numeric1", "passthrough", ["Body Type"]),
             (
                 "onehot_categorical",
                 OneHotEncoder(),
-                ["GenderMainDriver", "MaritalMainDriver", "Make", "Use", "PaymentMethod", "PaymentFrequency"],
+                ["LT_ANNUAL Flag", "UY_Newer", "Vehicle Make"]
             ),
         ],
         remainder='drop'
@@ -27,29 +27,37 @@ def build_model():
     tweedie_glm = Pipeline(
         [
             ("preprocessor", linear_model_preprocessor),
-            ("regressor", TweedieRegressor(power=1.9, alpha=1e-12, max_iter=300)),
+            # ("regressor", TweedieRegressor(power=1.9, alpha=1e-12, max_iter=300)),
+            ("regressor", GammaRegressor(alpha=1e-12, max_iter=300)),
         ]
     )
     tweedie_glm.fit(
-        df_train, df_train["Loss_Cost"], regressor__sample_weight=df_train["Exposure"]
-    )
-    #  joblib.dump(tweedie_glm, "Tweedie.sav")
+        df_train, df_train["Gross Cost"])
     return tweedie_glm
 
 
 def execute_model(tweedie_model, dataframe):
     y_pred = tweedie_model.predict(dataframe)
-    np.savetxt("Output\\output.csv", y_pred, delimiter=',')
+    dataframe["Output"] = y_pred
     dataframe.to_csv("Output\\df_test.csv")
 
 
-df = pd.read_csv("Output\\Policies.csv")
+df = pd.read_csv("Output\\Injury.csv")
+
 for col in df.columns:
     if "Unnamed" in col:
         df.drop(col, axis=1, inplace=True)
-
-df.set_index("PolicyReference", inplace=True, drop=True)
-df["Loss_Cost"] = df["Claim"]
-df_train, df_test = train_test_split(df, test_size=0.20, random_state=0)
+df_train = df
+df_test = df
+# df_train, df_test = train_test_split(df, test_size=0.33, random_state=0)
 glm = build_model()
 execute_model(glm, df_test)
+
+# index = 0
+# for index in range(len(df)):
+#     if index < len(df) - 1:
+#         if df["UY_Newer"].iloc[index] != "2012-17":
+#             df["UY_Newer"].iloc[index] = df["UY New"].iloc[index]
+#
+# df.to_csv("Output.csv")
+#
