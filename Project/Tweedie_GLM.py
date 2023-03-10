ea58ba830500bbe16baba039e89be5a58edaa323
s-jagannathan44 <85141510+s-jagannathan44@users.noreply.github.com>
2023-03-10 15:04:10 +0530

    Final Death and Injury Model

--- Project/Tweedie_GLM.py
+++ Project/Tweedie_GLM.py
@@ -1,10 +1,8 @@
 import pandas as pd
-# from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import OneHotEncoder
-# from sklearn.preprocessing import KBinsDiscretizer
+from sklearn.preprocessing import KBinsDiscretizer
 from sklearn.compose import ColumnTransformer
 from sklearn.pipeline import Pipeline
-# from sklearn.linear_model import TweedieRegressor
 from sklearn.linear_model import GammaRegressor
 
 
@@ -12,52 +10,66 @@ def build_model():
     global df_train, df_test
     linear_model_preprocessor = ColumnTransformer(
         [
-            # ("binned_numeric", KBinsDiscretizer(n_bins=3, strategy='uniform'), ["V AGE BAND"]),
-            ("passthrough_numeric", "passthrough", ["CC_desc"]),
-            ("passthrough_numeric1", "passthrough", ["Body Type"]),
+            ("binned_numeric", KBinsDiscretizer(n_bins=3, strategy='uniform'), ["V AGE NEW"]),
             (
                 "onehot_categorical",
                 OneHotEncoder(),
-                ["LT_ANNUAL Flag", "UY_Newer", "Vehicle Make"]
+                ["LT_ANNUAL Flag", "UY New", "CC_Make", "Zone_State", "Body Type"]
             ),
         ],
         remainder='drop'
     )
     print(df_test.shape, df_train.shape)
-    tweedie_glm = Pipeline(
+    gamma_glm = Pipeline(
         [
             ("preprocessor", linear_model_preprocessor),
             # ("regressor", TweedieRegressor(power=1.9, alpha=1e-12, max_iter=300)),
             ("regressor", GammaRegressor(alpha=1e-12, max_iter=300)),
         ]
     )
-    tweedie_glm.fit(
+    gamma_glm.fit(
         df_train, df_train["Gross Cost"])
-    return tweedie_glm
+    return gamma_glm
 
 
-def execute_model(tweedie_model, dataframe):
-    y_pred = tweedie_model.predict(dataframe)
+def execute_model(gamma_model, dataframe):
+    y_pred = gamma_model.predict(dataframe)
     dataframe["Output"] = y_pred
     dataframe.to_csv("Output\\df_test.csv")
 
 
-df = pd.read_csv("Output\\Injury.csv")
-
+df = pd.read_csv("Output\\Injury_NewApproach.csv")
 for col in df.columns:
     if "Unnamed" in col:
         df.drop(col, axis=1, inplace=True)
 df_train = df
 df_test = df
-# df_train, df_test = train_test_split(df, test_size=0.33, random_state=0)
+
 glm = build_model()
 execute_model(glm, df_test)
 
+# Major_States = ["Tamil Nadu", "Madhya Pradesh", "Kerala", "Karnataka", "Rajasthan", "Odisha", "West Bengal",
+#                 "Maharashtra", "Chattisgarh", "Telangana", "Uttar Pradesh", "Gujarat", "Andhra Pradesh",
+#                 "Bihar", "Haryana", "Delhi"]
+# list_states = df["Registration States"].unique()
+# OtherStates = []
+
+# for State in list_states:
+#     if Major_States.count(State) == 0:
+#         OtherStates.append(State)
+# for State in OtherStates:
+#     for index in range(len(df)):
+#         if index < len(df):
+#             if df["Registration States"].iloc[index] == State:
+#                 df["Registration States"].iloc[index] = df["Zone"].iloc[index]
+# df.to_csv("Output\\State.csv")
+#
+# df = pd.read_csv("Output\\Death.csv")
 # index = 0
 # for index in range(len(df)):
 #     if index < len(df) - 1:
 #         if df["UY_Newer"].iloc[index] != "2012-17":
 #             df["UY_Newer"].iloc[index] = df["UY New"].iloc[index]
 #
-# df.to_csv("Output.csv")
+# df.to_csv("Output\\Death_Modified.csv")
 #
