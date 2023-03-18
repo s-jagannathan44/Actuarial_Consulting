e5d259904a8813ff7537a508756bcdc5b4e0f373
s-jagannathan44 <85141510+s-jagannathan44@users.noreply.github.com>
2023-03-18 07:03:18 +0530

    Injury Gamma Model Completed and saved as sav file

--- Project/Gamma_GLM.py
+++ Project/Gamma_GLM.py
@@ -15,7 +15,7 @@ def build_model():
             (
                 "onehot_categorical",
                 OneHotEncoder(),
-              ["LT_ANNUAL Flag", "UY New", "CC_Make", "Zone_State", "Body Type"]
+                ["LT_ANNUAL Flag", "UY New", "CC_Make", "Zone_State", "Body Type"]
             ),
         ],
         remainder='drop'
@@ -29,8 +29,8 @@ def build_model():
         ]
     )
     gamma_glm.fit(
-        df_train, df_train["Loss Cost"])
-    # joblib.dump(gamma_glm, "Output\\GammaDeathModel.sav")
+        df_train, df_train["Loss Cost"], regressor__sample_weight=df_train["Exposure"])
+    joblib.dump(gamma_glm, "Output\\GammaInjuryModel.sav")
     return gamma_glm
 
 
@@ -40,17 +40,16 @@ def execute_model(gamma_model, dataframe):
     dataframe.to_csv("Output\\df_test.csv")
 
 
-df = pd.read_csv("Output\\Agro_GLM.csv")
+df = pd.read_csv("Output\\Injury Processed.csv")
 for col in df.columns:
     if "Unnamed" in col:
         df.drop(col, axis=1, inplace=True)
 df_train = df
 df_test = df
 
-glm = build_model()
-# glm = joblib.load("Output\\GammaDeathModel.sav")
+# glm = build_model()
+glm = joblib.load("Output\\GammaInjuryModel.sav")
 execute_model(glm, df_test)
-
 
 # UW Year Code
 # df = pd.read_csv("Output\\Death.csv")
