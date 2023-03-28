561a18fc9359f05466d2d4621c146a9c31362261
s-jagannathan44 <85141510+s-jagannathan44@users.noreply.github.com>
2023-03-28 16:25:09 +0530

    Added code for retrieving coefficients for Injury

--- Project/Gamma_GLM.py
+++ Project/Gamma_GLM.py
@@ -1,3 +1,4 @@
+import numpy as np
 import pandas as pd
 from sklearn.preprocessing import OneHotEncoder
 from sklearn.preprocessing import KBinsDiscretizer
@@ -5,17 +6,17 @@ from sklearn.compose import ColumnTransformer
 from sklearn.pipeline import Pipeline
 from sklearn.linear_model import GammaRegressor
 import joblib
-# import numpy as np
 
 
 def build_model():
+    global df_train, df_test
     linear_model_preprocessor = ColumnTransformer(
         [
             ("binned_numeric", KBinsDiscretizer(n_bins=3, strategy='uniform'), ["V AGE NEW"]),
             (
                 "onehot_categorical",
                 OneHotEncoder(),
-                ["LT_ANNUAL Flag", "UY New", "Body Type", "CC_desc", "Vehicle Make", "Zone_State"]
+                ["LT_ANNUAL Flag", "UY New", "CC_Make", "Zone_State", "Body Type"]
             ),
         ],
         remainder='drop'
@@ -24,13 +25,12 @@ def build_model():
     gamma_glm = Pipeline(
         [
             ("preprocessor", linear_model_preprocessor),
-            # ("regressor", TweedieRegressor(power=1.9, alpha=1e-12, max_iter=300)),
             ("regressor", GammaRegressor(alpha=1e-12, max_iter=300)),
         ]
     )
     gamma_glm.fit(
-        df_train, df["Loss Cost"], regressor__sample_weight=df["Exposure"])
-    #    joblib.dump(gamma_glm, "Output\\GammaDeathModel.sav")
+        df_train, df_train["Loss Cost"], regressor__sample_weight=df_train["Exposure"])
+    # joblib.dump(gamma_glm, "Output\\GammaDeathModel.sav")
     return gamma_glm, linear_model_preprocessor
 
 
@@ -51,7 +51,8 @@ def write_output(X_value):
         for col_name in col_names:
             col_list.append(col_name)
 
-    frame = pd.DataFrame(X_value, columns=col_list)
+    frame = pd.DataFrame(columns=col_list)
+    np.savetxt("Output\\Injury_co_efficient", X_value, delimiter=",")
     frame.to_csv("Output\\Output.csv")
 
 
@@ -61,19 +62,15 @@ def execute_model(gamma_model, dataframe):
     dataframe.to_csv("Output\\df_test.csv")
 
 
-df = pd.read_csv("Output\\Death_M_Clubbed.csv")
-
-for col_ in df.columns:
-    if "Unnamed" in col_:
-        df.drop(col_, axis=1, inplace=True)
+df = pd.read_csv("Output\\Injury_M_Clubbed.csv")
+for col in df.columns:
+    if "Unnamed" in col:
+        df.drop(col, axis=1, inplace=True)
 df_train = df
 df_test = df
 
-glm, transformer = build_model()
-column_dict = get_columns()
-# X = transformer.fit_transform(df_train).toarray()
-# write_output(X)
-# np.savetxt("Output\\co_efficient.csv", glm._final_estimator.coef_, delimiter=",")
-
-glm_ = joblib.load("Output\\GammaMultiplierDeathModel.sav")
-execute_model(glm_, df_test)
+# glm, transformer = build_model()
+# column_dict = get_columns()
+# write_output(glm._final_estimator.coef_)
+glm = joblib.load("GammaMultiplierInjuryModel.sav")
+execute_model(glm, df_test)
