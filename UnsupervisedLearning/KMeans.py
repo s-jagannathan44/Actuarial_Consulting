05f36799ee6fde915770b8d759f5faf22c33fd91
s-jagannathan44 <85141510+s-jagannathan44@users.noreply.github.com>
2023-04-17 13:05:58 +0530

    Added code to perform feature selection via PFA

--- UnsupervisedLearning/KMeans.py
+++ UnsupervisedLearning/KMeans.py
@@ -1,30 +1,61 @@
+from collections import defaultdict
 import pandas as pd
 import matplotlib.pyplot as plt
 from kneed import KneeLocator
 from sklearn.cluster import KMeans as Km
 from sklearn.decomposition import PCA
-from sklearn.metrics import silhouette_score
+from sklearn.metrics import silhouette_score, euclidean_distances
 from sklearn.preprocessing import MinMaxScaler
 import joblib
 import numpy as np
 # import seaborn as sns
 
 
+class PFA(object):
+    def __init__(self, n_features, q=None):
+        self.features_ = None
+        self.indices_ = None
+        self.q = q
+        self.n_features = n_features
+
+    def fit(self, X):
+        if not self.q:
+            self.q = X.shape[1]
+
+        sc = MinMaxScaler()
+        X = sc.fit_transform(X)
+
+        pca = PCA(n_components=self.q).fit(X)  # calculate Covmatrix is embedded in PCA
+        A_q = pca.components_.T
+
+        kmeans = Km(n_clusters=self.n_features,  max_iter=100, init='k-means++').fit(A_q)
+        clusters = kmeans.predict(A_q)
+        cluster_centers = kmeans.cluster_centers_
+
+        dists = defaultdict(list)
+        for i, c in enumerate(clusters):
+            dist = euclidean_distances([A_q[i, :]], [cluster_centers[c, :]])[0][0]
+            dists[c].append((i, dist))
+
+        self.indices_ = [sorted(f, key=lambda x: x[1])[0][0] for f in dists.values()]
+        self.features_ = X[:, self.indices_]
+
+
 def cluster_data():
     # Run local implementation of k means
-    km = Km(n_clusters=n_clusters, max_iter=100, init='k-means++')
-    km.fit(X_std)
-    joblib.dump(km, "Output\\Cluster.sav")
-    # km = joblib.load("Output\\Cluster.sav")
+    # km = Km(n_clusters=n_clusters, max_iter=100, init='k-means++')
+    # km.fit(X_std)
+    # joblib.dump(km, "Output\\Cluster.sav")
+    km = joblib.load("Output\\NRA HB80120.sav")
     labels = km.predict(X_std)
     print("Predict complete")
-    print(silhouette_score(X_std, labels))
+    # print(silhouette_score(X_std, labels))
     df["Cluster"] = labels  # km.labels_
     df.to_csv("Output\\clustered.csv")
 
     # plt.figure(fig size=(16, 6))
     # colors = ["#A181E0", "#E08181", "#599988", "#FF0000"]
-    # ax = sns.scatter plot(data=df, x="A80", y="NRA by Distance", hue='Cluster',
+    # ax = sns.scatter plot(data=df, x="NRA by Distance", y="NHB by Distance", hue='Cluster',
     #                      s=200, palette=colors, legend=True)
     #
     # plt.legend(loc='lower right', title='Cluster')
@@ -90,7 +121,10 @@ def explained_variance():
 # -------------------- CODE STARTS HERE ---------------------------------------
 sse = []
 sil_scores = []
-df = pd.read_csv("Output\\InputFile -All.csv")
+df = pd.read_csv("Output\\InputFile.csv")
+# pfa = PFA(n_features=6)
+# pfa.fit(df)
+# print(pfa.indices_)
 X_std = MinMaxScaler().fit_transform(df)
 
 # explained_variance()
