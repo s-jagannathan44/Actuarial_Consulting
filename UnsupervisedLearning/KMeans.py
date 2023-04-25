e069b4ecd46950f9de579a2211227fa19e379255
s-jagannathan44 <85141510+s-jagannathan44@users.noreply.github.com>
2023-04-25 11:55:28 +0530

    Added random sampling code

--- UnsupervisedLearning/KMeans.py
+++ UnsupervisedLearning/KMeans.py
@@ -6,11 +6,31 @@ from sklearn.cluster import KMeans as Km
 from sklearn.preprocessing import MinMaxScaler
 import joblib
 import seaborn as sns
+# from sklearn.model_selection import train_test_split
+
+
+def plot3d(x, y_clusters):
+    fig = plt.figure(figsize=(15, 15))
+    ax = fig.add_subplot(111, projection='3d')
+    ax.scatter(x[y_clusters == 0, 0], x[y_clusters == 0, 2], x[y_clusters == 0, 1], s=40, color='blue',
+               label="cluster 0")
+    ax.scatter(x[y_clusters == 1, 0], x[y_clusters == 1, 2], x[y_clusters == 1, 1], s=40, color='orange',
+               label="cluster 1")
+    ax.scatter(x[y_clusters == 2, 0], x[y_clusters == 2, 2], x[y_clusters == 2, 1], s=40, color='green',
+                label="cluster 2")
+    ax.scatter(x[y_clusters == 3, 0], x[y_clusters == 3, 2], x[y_clusters == 3, 1], s=40, color='#D12B60',
+                label="cluster 3")
+
+    ax.set_xlabel('NRA by Distance')
+    ax.set_ylabel('NHB by distance')
+    ax.set_zlabel('A120 by distance')
+    ax.legend()
+    plt.show()
 
 
 def plot(X, y):
     plt.figure(figsize=(16, 6))
-    colors = ["#FFFF00", "#FF0000", "#599988", "#E08181"]
+    colors = ["#FFFF00", "#FF0000", "#599988", "#E08181", "#000000"]
     ax = sns.scatterplot(data=df, x=X, y=y, hue='Cluster',
                          s=200, palette=colors, legend=True)
     plt.legend(loc='lower right', title='Cluster')
@@ -18,6 +38,10 @@ def plot(X, y):
 
 
 def cluster_data():
+    # km = Km(n_clusters=n_clusters, max_iter=100, init='k-means++')
+    # km.fit(X_std)
+    # joblib.dump(km, "Output\\Cluster.sav")
+
     km = joblib.load("Output\\ModelFiles\\NRANHB1204Clusters.sav")
     labels = km.predict(X_std)
     print("Predict complete")
@@ -25,8 +49,11 @@ def cluster_data():
     df["Cluster"] = labels  # km.labels_
     df.to_csv("Output\\Clustered\\clustered.csv")
 
-    plot("NRA by Distance", "NHB by Distance")
-    plt.show()
+    # plot("NRA by Distance", "NHB by Distance")
+    # plot("NRA by Distance", "A 120")
+    # plot("NHB by Distance", "A 120")
+    # plt.show()
+    plot3d(X_std, labels)
 
 
 def elbow_method():
@@ -46,7 +73,14 @@ def elbow_method():
 
 # -------------------- CODE STARTS HERE ---------------------------------------
 sse = []
-df = pd.read_csv("Output\\InputFile_March.csv")
+# df = pd.read_csv("Output\\Clustered\\Out of Sample\\RandomSampling\\InputFile_Test.csv")
+df = pd.read_csv("Output\\InputFile.csv")
 X_std = MinMaxScaler().fit_transform(df)
-
+# n_clusters = elbow_method()
+# print(n_clusters)
+# n_clusters = 4
 cluster_data()
+
+# X_train, X_test, y_train, y_test = train_test_split(df, df, test_size=0.5, random_state=25)
+# X_train.to_csv("Output\\Clustered\\Out of Sample\\RandomSampling\\InputFile_Fit.csv")
+# X_test.to_csv("Output\\Clustered\\Out of Sample\\RandomSampling\\InputFile_Test.csv")
