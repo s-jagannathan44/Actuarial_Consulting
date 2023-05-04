1091ce4afed766337967be6d166ee18f12b1c6d2
s-jagannathan44 <85141510+s-jagannathan44@users.noreply.github.com>
2023-05-04 07:53:57 +0530

    Final code for PCV taxi to be delivered to Client

--- UnsupervisedLearning/KMeans.py
+++ UnsupervisedLearning/KMeans.py
@@ -1,3 +1,6 @@
+# This file contains the code for clustering Commercial Vehicle - Taxi Data
+
+# Import statements used to include code from other libraries
 import pandas as pd
 import matplotlib.pyplot as plt
 from kneed import KneeLocator
@@ -6,26 +9,30 @@ from sklearn.metrics import silhouette_score
 from sklearn.preprocessing import MinMaxScaler
 import joblib
 import seaborn as sns
-# from sklearn.model_selection import train_test_split
 
 
+# This method is for plotting the clusters in a 3-dimensional scatter plot
 def plot3d(x, y_clusters):
     fig = plt.figure(figsize=(15, 15))
     ax = fig.add_subplot(111, projection='3d')
+
+    # There is one line for each cluster to be plotted. As we have 6 clusters we have 6 lines.
+    # In case fewer clusters are used the necessary changes can be made
     ax.scatter(x[y_clusters == 0, 0], x[y_clusters == 0, 2], x[y_clusters == 0, 1], s=40, color='red',
                label="cluster 0")
     ax.scatter(x[y_clusters == 1, 0], x[y_clusters == 1, 2], x[y_clusters == 1, 1], s=40, color='blue',
                label="cluster 1")
     ax.scatter(x[y_clusters == 2, 0], x[y_clusters == 2, 2], x[y_clusters == 2, 1], s=40, color='green',
-                label="cluster 2")
+               label="cluster 2")
     ax.scatter(x[y_clusters == 3, 0], x[y_clusters == 3, 2], x[y_clusters == 3, 1], s=40, color='black',
-                label="cluster 3")
+               label="cluster 3")
 
     ax.scatter(x[y_clusters == 4, 0], x[y_clusters == 4, 2], x[y_clusters == 4, 1], s=40, color='yellow',
                label="cluster 4")
     ax.scatter(x[y_clusters == 5, 0], x[y_clusters == 5, 2], x[y_clusters == 5, 1], s=40, color='orange',
                label="cluster 5")
 
+    # These are the 3 columns in the input file which are clustered
     ax.set_xlabel('NRA by Distance')
     ax.set_ylabel('NHB by distance')
     ax.set_zlabel('A120 by distance')
@@ -33,8 +40,11 @@ def plot3d(x, y_clusters):
     plt.show()
 
 
+# This method is for plotting the clusters in a 2-dimensional scatter plot
 def plot(X, y):
     plt.figure(figsize=(16, 6))
+
+    # The number of  colors should be equal to the number of  clusters and should be specified in RGB format.
     colors = ["#FFFF00", "#FF0000", "#599988", "#E08181", "#F00000", "#ABCDEF"]
     ax = sns.scatterplot(data=df, x=X, y=y, hue='Cluster',
                          s=200, palette=colors, legend=True)
@@ -42,25 +52,39 @@ def plot(X, y):
     ax.set_title("Clustered Points", fontsize='xx-large', y=1.05)
 
 
-def cluster_data():
-    # km = Km(n_clusters=n_clusters, max_iter=100, init='k-means++')
-    # km.fit(X_std)
-    # joblib.dump(km, "Output\\Cluster.sav")
+# This method is used for fitting the clusters and should not be called
+# once the model has been finalised and saved
+def fit_data():
+    OutputFileName = "Output\\Cluster.sav"
+    km = Km(n_clusters=n_clusters, max_iter=100, init='k-means++')
+    km.fit(X_std)
+    # This line saves the fitted model to file.
+    # Once model is finalised this file will be used to cluster the data
+    joblib.dump(km, OutputFileName)
 
-    km = joblib.load("CommercialVehicle\\Clustered\\6Clusters.sav")
+
+# This method is used for generating the clusters from the saved file
+def cluster_data():
+    ModelFileName = "Output\\Cluster.sav"
+
+    # Below code loads the model from file, calculates the silhouette_score after generating the clusters
+    # and finally plots them in 2 and 3 dimensions
+    km = joblib.load(ModelFileName)
     labels = km.predict(X_std)
     print("Predict complete")
     print(silhouette_score(X_std, labels))
     df["Cluster"] = labels  # km.labels_
     df.to_csv("Output\\Clustered\\clustered.csv")
-    # plot("NRA by Distance", "NHB by Distance")
-    #  plot("NRA by Distance", "A 120")
-    # plot("NHB by Distance", "A 120")
+    plot("NRA by Distance", "NHB by Distance")
+    plot("NRA by Distance", "A 120")
+    plot("NHB by Distance", "A 120")
     plt.show()
-    # plot3d(X_std, labels)
+    plot3d(X_std, labels)
 
 
+# This method is used for calculating the initial number of clusters using the elbow method
 def elbow_method():
+    sse = []
     list_k = list(range(2, 12))
     for k in list_k:
         km = Km(n_clusters=k)
@@ -72,18 +96,16 @@ def elbow_method():
     plt.xlabel(r'Number of clusters $k$')
     plt.ylabel('Sum of squared distance')
     plt.show()
+    # sometimes the elbow is hard to identify visually so this library returns us the number of clusters
     return KneeLocator(range(2, 12), sse, curve="convex", direction="decreasing").elbow
 
 
-# -------------------- CODE STARTS HERE ---------------------------------------
-sse = []
+# -------------------- MAIN CODE STARTS HERE ---------------------------------------
+# This is the input file from which the clusters are to be generated
 df = pd.read_csv("CommercialVehicle\\InputFile.csv")
+# this line is used for Scaling  the input before clustering
 X_std = MinMaxScaler().fit_transform(df)
-# n_clusters = elbow_method()
-# print(n_clusters)
-# n_clusters = 6
+# This line sets the number of clusters to be used by K Means
+n_clusters = 6
+fit_data()
 cluster_data()
-
-# X_train, X_test, y_train, y_test = train_test_split(df, df, test_size=0.5, random_state=25)
-# X_train.to_csv("Output\\Clustered\\Out of Sample\\RandomSampling\\InputFile_Fit.csv")
-# X_test.to_csv("Output\\Clustered\\Out of Sample\\RandomSampling\\InputFile_Test.csv")
