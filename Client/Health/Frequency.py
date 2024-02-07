4577f1759372aaac9e1fd854c17291088b35a595
s-jagannathan44 <jagannathan.s@gmail.com>
2024-02-07 16:12:56 +0530

    Aborted sklearn code that draws pivots

--- Client/Health/Frequency.py
+++ Client/Health/Frequency.py
@@ -1,12 +1,15 @@
 import joblib
 import pandas as pd
+from sklearn.metrics import d2_tweedie_score
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import OneHotEncoder, OrdinalEncoder
 from sklearn.compose import ColumnTransformer
 from sklearn.pipeline import Pipeline
 from sklearn.linear_model import TweedieRegressor
+from sklearn.metrics import log_loss
 
 
+# ('ordinal_categorical', OrdinalEncoder(), ["Financial_Year"]),
 def build_model():
     global df_train, df_test
     linear_model_preprocessor = ColumnTransformer(
@@ -14,9 +17,8 @@ def build_model():
             (
                 "onehot_categorical",
                 OneHotEncoder(),
-                "Mem_Age_New Mem_Gender_New Renewal_Count_New".split()
+                "Revised_Individual_Floater_New".split()
             ),
-            ('ordinal_categorical', OrdinalEncoder(), ["Financial_Year"]),
         ],
         remainder='drop'
         # remainder ='passthrough'
@@ -35,53 +37,64 @@ def build_model():
     return tweedie_glm
 
 
+def make_pivots(dataframe, columns):
+    df2 = pd.pivot_table(dataframe, values="PAID_AMT Pred_Cost LIVES_EXPOSED".split(), columns=columns,
+                         aggfunc="sum")
+    df2 = df2.transpose()
+    df2["Actual"] = df2["PAID_AMT"] / df2["LIVES_EXPOSED"]
+    df2["Predicted"] = df2["Pred_Cost"] / df2["LIVES_EXPOSED"]
+    df2["Error"] = (df2["Actual"] - df2["Predicted"]) / df2["Actual"]
+    df2.to_csv("Output\\" + columns + ".csv")
+
+
+def make_multi(dataframe, columns):
+    df2 = pd.pivot_table(dataframe, values="PAID_AMT Pred_Cost LIVES_EXPOSED".split(), columns=columns,
+                         aggfunc="sum")
+    df2 = df2.transpose()
+    df2["Actual"] = df2["PAID_AMT"] / df2["LIVES_EXPOSED"]
+    df2["Predicted"] = df2["Pred_Cost"] / df2["LIVES_EXPOSED"]
+    df2["Error"] = (df2["Actual"] - df2["Predicted"]) / df2["Actual"]
+    df2.to_csv("Output\\multi.csv")
+
+
 def execute_model(tweedie_model, dataframe):
-    dataframe.to_csv("Output\\text.csv")
+    # dataframe.to_csv("Output\\text.csv")
     y_pred = tweedie_model.predict(dataframe)
     dataframe["Pred"] = y_pred
+    dataframe["Pred_Cost"] = dataframe["Pred"] * dataframe["LIVES_EXPOSED"]
     dataframe.to_csv("Output\\text_out.csv")
+    # print(d2_tweedie_score(dataframe["Loss_Cost"], y_pred, power=1.9, sample_weight=dataframe["LIVES_EXPOSED"]))
+    print(log_loss(dataframe["Loss_Cost"], y_pred, sample_weight=dataframe["LIVES_EXPOSED"]))
+    make_pivots(dataframe, "Revised_Individual_Floater_New")
+    # make_pivots(dataframe, "Product_Name_New")
+    # make_pivots(dataframe, "Zone")
+    # make_multi(dataframe, "Zone Product_Name_New Revised_Individual_Floater_New".split())
+
+
+def othering(dataframe):
+    make_pivots(dataframe, "Revised_Individual_Floater_New")
+    make_pivots(dataframe, "Product_Name_New")
+    make_pivots(dataframe, "Zone")
+    make_pivots(dataframe, "Mem_Age_New")
+    make_pivots(dataframe, "Mem_Gender_New")
+    make_pivots(dataframe, "Renewal_Count_New")
+    make_pivots(dataframe, "Channel_type_New")
+    make_pivots(dataframe, "Sum_Insured_New")
 
 
 df = pd.read_csv("CSV\\FrequencyModelFile.csv")
+
+# train, df = train_test_split(input_file, test_size=0.2, random_state=0)
+# df.to_csv("Output\\OOS.csv")
 for col in df.columns:
     if "Unnamed" in col:
         df.drop(col, axis=1, inplace=True)
 df = df[df["LIVES_EXPOSED"] >= 1]
+df = df[(df['PAID_AMT'] != 0) & (df['PAID_AMT'] != -851)]
+df = df[~ df["Product_Name_New"].isin("SURPLUS-FLOATER".split())]
+df = df[df["Financial_Year"].isin("FY18 FY19 FY20 FY22 FY23".split())]
 df["Loss_Cost"] = df["PAID_AMT"] / df["LIVES_EXPOSED"]
-df = df[df["Loss_Cost"] >= 0]
 df["Loss_Cost"].fillna(0, inplace=True)
 df_train, df_test = train_test_split(df, test_size=0.2, random_state=0)
 glm = build_model()
 execute_model(glm, df_test)
-
-
-def pivot_age(dataframe):
-    df2 = pd.pivot_table(dataframe, values="Loss_Cost Pred LIVES_EXPOSED".split(), columns="Mem_Age_New",
-                         aggfunc="mean")
-    df2 = df2.transpose()
-    df2["Error"] = (df2["Loss_Cost"] - df2['Pred']) / df2["Loss_Cost"]
-    df2.to_csv("Output\\Age.csv")
-
-
-def pivot_gender(dataframe):
-    df2 = pd.pivot_table(dataframe, values="Loss_Cost Pred LIVES_EXPOSED".split(), columns="Mem_Gender_New",
-                         aggfunc="mean")
-    df2 = df2.transpose()
-    df2["Error"] = (df2["Loss_Cost"] - df2['Pred']) / df2["Loss_Cost"]
-    df2.to_csv("Output\\Gender.csv")
-
-
-def pivot_renewal_count(dataframe):
-    df2 = pd.pivot_table(dataframe, values="Loss_Cost Pred LIVES_EXPOSED".split(), columns="Renewal_Count_New",
-                         aggfunc="mean")
-    df2 = df2.transpose()
-    df2["Error"] = (df2["Loss_Cost"] - df2['Pred']) / df2["Loss_Cost"]
-    df2.to_csv("Output\\RC.csv")
-
-
-def pivot_financial_year(dataframe):
-    df2 = pd.pivot_table(dataframe, values="Loss_Cost Pred LIVES_EXPOSED".split(), columns="Financial_Year",
-                         aggfunc="mean")
-    df2 = df2.transpose()
-    df2["Error"] = (df2["Loss_Cost"] - df2['Pred']) / df2["Loss_Cost"]
-    df2.to_csv("Output\\RC.csv")
