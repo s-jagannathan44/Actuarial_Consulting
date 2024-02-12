9bf37035e0cff8e3698264ac8dcfc0911db674c1
s-jagannathan44 <jagannathan.s@gmail.com>
2024-02-12 14:59:34 +0530

    final model

--- Client/Health/Frequency.py
+++ Client/Health/Frequency.py
@@ -1,7 +1,7 @@
 import joblib
 import pandas as pd
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import OneHotEncoder, PolynomialFeatures
+from sklearn.preprocessing import OneHotEncoder, PolynomialFeatures, OrdinalEncoder
 from sklearn.compose import ColumnTransformer
 from sklearn.pipeline import Pipeline, make_pipeline
 from sklearn.linear_model import TweedieRegressor
@@ -12,18 +12,15 @@ def build_model():
 
     interaction = make_pipeline(
         OneHotEncoder(),
-        PolynomialFeatures(degree=3, interaction_only=True, include_bias=False)
+        PolynomialFeatures(degree=2, interaction_only=True, include_bias=False)
     )
-    # interactive_2 = make_pipeline(OneHotEncoder(),
-    #                               PolynomialFeatures(degree=2, interaction_only=True, include_bias=False))
-    non_interactive = make_pipeline(OneHotEncoder())
-
+    # non_interactive = make_pipeline(OneHotEncoder())
+# "Revised_Product_Name_New Mem_Gender_New Mem_Age_New Renewal_Count_New Financial_Year ".split()),
     column_trans = ColumnTransformer(
         [
             ('interaction', interaction,
-             "Revised_Product_Name_New Mem_Gender_New Mem_Age_New Renewal_Count_New".split()),
-            # ('interactive_2', interactive_2, "Mem_Age_New Renewal_Count_New".split()),
-            # ('non_interactive', non_interactive, "Zone_New Channel_type_New Sum_Insured_New".split()),
+             "Mem_Age_New Mem_Gender_New Revised_Product_Name_New Renewal_Count_New Financial_Year ".split()),
+            # ('non_interactive', non_interactive, "Financial_Year".split()),
         ],
     )
 
@@ -31,7 +28,7 @@ def build_model():
     tweedie_glm = Pipeline(
         [
             ("transform", column_trans),
-            ("regressor", TweedieRegressor(power=1.9, alpha=1e-12, max_iter=1000)),
+            ("regressor", TweedieRegressor(power=1.9, alpha=1e-12, max_iter=5000)),
         ]
     )
     tweedie_glm.fit(
@@ -62,27 +59,49 @@ def make_multi(dataframe, columns):
 
 
 def execute_model(tweedie_model, dataframe):
-    # dataframe.to_csv("Output\\text.csv")
     y_pred = tweedie_model.predict(dataframe)
     dataframe["Pred"] = y_pred
     dataframe["Pred_Cost"] = dataframe["Pred"] * dataframe["LIVES_EXPOSED"]
     dataframe.to_csv("Output\\text_out.csv")
-    # make_pivots(dataframe, "Revised_Individual_Floater_New")
-    # make_pivots(dataframe, "Mem_Gender_New")
-    # make_pivots(dataframe, "Sum_Insured_New")
-    make_multi(dataframe, "Revised_Product_Name_New Mem_Gender_New Mem_Age_New Renewal_Count_New"
-                          .split())
+    make_multi(dataframe, "Mem_Age_New Mem_Gender_New Revised_Product_Name_New Renewal_Count_New  Financial_Year"
+               .split())
 
 
-def othering(dataframe):
-    make_pivots(dataframe, "Revised_Individual_Floater_New")
-    # make_pivots(dataframe, "Product_Name_New")
-    make_pivots(dataframe, "Zone_New")
-    make_pivots(dataframe, "Mem_Age_New")
-    make_pivots(dataframe, "Mem_Gender_New")
-    # make_pivots(dataframe, "Renewal_Count_New")
-    # make_pivots(dataframe, "Channel_type_New")
-    # make_pivots(dataframe, "Sum_Insured_New")
+def multiplier(year):
+    if year == "FY23":
+        return 1.07
+    elif year == "FY22":
+        return 1.016
+    else:
+        return 1.0
+
+
+df = pd.read_csv("CSV\\FrequencyModelFile.csv", usecols="Mem_Age_New Mem_Gender_New LIVES_EXPOSED "
+                                                        "PAID_AMT Financial_Year Renewal_Count_New Zone_New "
+                                                        "Revised_Product_Name_New Channel_type_New Sum_Insured_New "
+                 .split())
+for col in df.columns:
+    if "Unnamed" in col:
+        df.drop(col, axis=1, inplace=True)
+df = df[df["LIVES_EXPOSED"] >= 1]
+df = df[~ df["Revised_Product_Name_New"].isin("SURPLUS-FLOATERFLOATER SURPLUS-FLOATERMED-PLT-046 "
+                                              "SURPLUS-INDINDIVIDUAL Corona Kavach PolicyFLOATER "
+                                              "Corona Kavach PolicyINDIVIDUAL"
+                                              " Star Hospital Cash Insurance PolicyFLOATER ".split())]
+df = df[df["Financial_Year"].isin("FY18 FY19 FY20 FY22 FY23".split())]
+df["PAID_AMT"] = df["PAID_AMT"] * df["Financial_Year"].apply(lambda x: multiplier(x))
+df["Loss_Cost"] = df["PAID_AMT"] / df["LIVES_EXPOSED"]
+df["Loss_Cost"].fillna(0, inplace=True)
+df["Mem_Age_New"].fillna("0.0", inplace=True)
+df["Pred_Cost"] = df["Loss_Cost"]
+
+df_train, df_test = train_test_split(df, test_size=0.2, random_state=0)
+df_model = df_train[['Mem_Age_New', "Mem_Gender_New", "Revised_Product_Name_New", "Financial_Year", "Renewal_Count_New"
+                     ]]
+
+# df_model["Mem_Age_New"] = df_model["Mem_Age_New"].astype(str)
+glm = build_model()
+execute_model(glm, df_test)
 
 
 def find_separation():
@@ -96,25 +115,12 @@ def find_separation():
     df2.to_csv("Output\\multi.csv")
 
 
-df = pd.read_csv("CSV\\FrequencyModelFile.csv", usecols="Mem_Age_New Mem_Gender_New LIVES_EXPOSED "
-                                                        "PAID_AMT Financial_Year Renewal_Count_New Zone_New "
-                                                        "Revised_Product_Name_New Channel_type_New Sum_Insured_New "
-                 .split())
-for col in df.columns:
-    if "Unnamed" in col:
-        df.drop(col, axis=1, inplace=True)
-df = df[df["LIVES_EXPOSED"] >= 1]
-# df = df[(df['PAID_AMT'] != 0) & (df['PAID_AMT'] != -851)]
-df = df[~ df["Revised_Product_Name_New"].isin("SURPLUS-FLOATERFLOATER SURPLUS-FLOATERMED-PLT-046 "
-                                              "SURPLUS-INDINDIVIDUAL Corona Kavach PolicyFLOATER "
-                                              "Corona Kavach PolicyINDIVIDUAL"
-                                              " Star Hospital Cash Insurance PolicyFLOATER ".split())]
-df = df[df["Financial_Year"].isin("FY18 FY19 FY20 FY22 FY23".split())]
-df["Loss_Cost"] = df["PAID_AMT"] / df["LIVES_EXPOSED"]
-df["Loss_Cost"].fillna(0, inplace=True)
-df["Pred_Cost"] = df["Loss_Cost"]
-df_train, df_test = train_test_split(df, test_size=0.2, random_state=0)
-df_model = df_train[['Mem_Age_New', "Mem_Gender_New", "Revised_Product_Name_New", "Renewal_Count_New"
-                     ]]
-glm = build_model()
-execute_model(glm, df_test)
+def othering(dataframe):
+    make_pivots(dataframe, "Revised_Individual_Floater_New")
+    # make_pivots(dataframe, "Product_Name_New")
+    make_pivots(dataframe, "Zone_New")
+    make_pivots(dataframe, "Mem_Age_New")
+    make_pivots(dataframe, "Mem_Gender_New")
+    # make_pivots(dataframe, "Renewal_Count_New")
+    # make_pivots(dataframe, "Channel_type_New")
+    # make_pivots(dataframe, "Sum_Insured_New")
