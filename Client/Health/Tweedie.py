4d225bd2d8bc37593d8186dea596b7072bbbbcb7
s-jagannathan44 <jagannathan.s@gmail.com>
2024-02-15 13:29:15 +0530

    XgBoost Tweedie

--- Client/Health/Tweedie.py
+++ Client/Health/Tweedie.py
@@ -1,10 +1,13 @@
 import pandas as pd
+from matplotlib import pyplot as plt
 from sklearn.metrics import mean_absolute_error
 from sklearn.model_selection import train_test_split, GridSearchCV
 from xgboost import XGBRegressor
 
+from xgboost import plot_importance
+
 passthrough_list = ["LIVES_EXPOSED"]
-ordinal_list = ["Mem_Age_New", "Mem_Gender_New", "Revised_Product_Name_New", "Financial_Year", "Renewal_Count_New",
+ordinal_list = ["Mem_Age_New", "Mem_Gender_New", "Revised_Product_Name_New", "Renewal_Count_New", "Financial_Year",
                 "Zone_New", "Sum_Insured_New"]
 
 
@@ -17,7 +20,6 @@ def return_best_model(estimator):
     mae_base = mean_absolute_error(y_test, y_pred_base)
     print(f'Mean Baseline: {mean_baseline:.1f} ')
     print(f'Baseline mean absolute error: {mae_base}')
-    # print(f'r2 score: {r2_base}')
     # defining parameter range
     param_grid = {
         'n_estimators': [1000, 3000],
@@ -52,22 +54,22 @@ def make_multi(dataframe, columns):
     below_ten = df2[df2["Error"] <= 0.1]["LIVES_EXPOSED"].sum()
     total = df2["LIVES_EXPOSED"].sum()
     print('{:.2%}'.format(below_ten / total))
-    df2.to_csv("Output\\multi.csv")
+    # df2.to_csv("Output\\multi.csv")
 
 
 def execute_model(tweedie_model, test_set, dataframe):
     y_pred = tweedie_model.predict(test_set)
     dataframe["Pred"] = y_pred
     dataframe["Pred_Cost"] = dataframe["Pred"] * dataframe["LIVES_EXPOSED"]
-    dataframe.to_csv("Output\\text_out.csv")
-    make_multi(dataframe, "Mem_Age_New Mem_Gender_New Revised_Product_Name_New Renewal_Count_New  Financial_Year "
-                          "Zone_New Sum_Insured_New".split())
+    # dataframe.to_csv("Output\\text_out.csv")
+    make_multi(dataframe, "Mem_Age_New Mem_Gender_New Revised_Product_Name_New Renewal_Count_New  "
+                          "Financial_Year Zone_New Sum_Insured_New".split())
 
 
 # -------------------- CODE STARTS HERE ---------------------------------------
-df = pd.read_csv("FrequencyModelFile.csv", usecols="Mem_Age_New Mem_Gender_New LIVES_EXPOSED "
-                                                   "PAID_AMT Financial_Year Renewal_Count_New Zone_New "
-                                                   "Revised_Product_Name_New Sum_Insured_New "
+df = pd.read_csv("CSV\\FrequencyModelFile.csv", usecols="Mem_Age_New Mem_Gender_New LIVES_EXPOSED "
+                                                        "Financial_Year PAID_AMT Renewal_Count_New "
+                                                        "Revised_Product_Name_New Zone_New Sum_Insured_New"
                  .split())
 
 for col in df.columns:
@@ -80,6 +82,7 @@ df = df[~ df["Revised_Product_Name_New"].isin("SURPLUS-FLOATERFLOATER SURPLUS-FL
                                               "Corona Kavach PolicyINDIVIDUAL"
                                               " Star Hospital Cash Insurance PolicyFLOATER ".split())]
 df = df[df["Financial_Year"].isin("FY18 FY19 FY20 FY22 FY23".split())]
+
 df["PAID_AMT"] = df["PAID_AMT"] * df["Financial_Year"].apply(lambda x: multiplier(x))
 df["Loss_Cost"] = df["PAID_AMT"] / df["LIVES_EXPOSED"]
 df["Loss_Cost"].fillna(0, inplace=True)
@@ -89,24 +92,24 @@ for col in ordinal_list:
 
 df_train, df_test_val, y_train, y_test_val = train_test_split(df, df["Loss_Cost"], test_size=0.30, random_state=40)
 df_test, df_val, y_test, y_val = train_test_split(df_test_val, y_test_val, test_size=0.33, random_state=40)
-X_train = df_train.drop(["PAID_AMT", "Loss_Cost"], axis=1)
-X_test = df_test.drop(["PAID_AMT", "Loss_Cost"], axis=1)
-X_val = df_val.drop(["PAID_AMT", "Loss_Cost"], axis=1)
-# df.head(1).to_csv("test.csv")
-# rgr = XGBRegressor(objective='reg:tweedie', seed=42, eval_metric='tweedie-nloglik@1.2', n_estimators=100,
-#                    max_depth=3, learning_rate=0.1, colsample_bytree=0.6, tweedie_variance_power=1.9)
+exposure = df_train["LIVES_EXPOSED"],
+X_train = df_train.drop(["PAID_AMT", "Loss_Cost", "LIVES_EXPOSED"], axis=1)
+X_test = df_test.drop(["PAID_AMT", "Loss_Cost", "LIVES_EXPOSED"], axis=1)
+X_val = df_val.drop(["PAID_AMT", "Loss_Cost", "LIVES_EXPOSED"], axis=1)
 
-rgr = XGBRegressor(objective='reg:tweedie', seed=42, eval_metric='tweedie-nloglik@1.7',
-                   tweedie_variance_power=1.7, enable_categorical=True, max_cat_to_onehot=1)
+rgr = XGBRegressor(objective='reg:tweedie', tweedie_variance_power=1.5, n_estimators=500,
+                   enable_categorical=True)
 
-params_dict = {'sample_weight': X_train["LIVES_EXPOSED"], 'verbose': True}
-# rgr.fit(X_train, y_train, **params_dict)
-rgr = return_best_model(rgr)
-rgr.save_model('Output\\model.json')
+params_dict = {'sample_weight': exposure, 'verbose': True}
+rgr.fit(X_train, y_train, **params_dict)
+# rgr = return_best_model(rgr)
 execute_model(rgr, X_test, df_test)
 
-# rgr = XGBRegressor()
-# rgr.load_model('Output\\model.json')
-# X_test = pd.read_csv("test.csv")
-# for col in ordinal_list:
-#     X_test[col] = X_test[col].astype('category')
+(pd.Series(rgr.feature_importances_,
+           index=["Mem_Age_New", "Mem_Gender_New", "Revised_Product_Name_New", "Financial_Year", "Renewal_Count_New",
+                  "Zone_New", "Sum_Insured_New"])
+ .nlargest(7)
+ .plot(kind='barh'))
+# plot feature importance
+plot_importance(rgr)
+plt.show()
