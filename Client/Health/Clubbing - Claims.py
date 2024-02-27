d02d9e49555af43ac9e42be28e88438598d06a7f
s-jagannathan44 <jagannathan.s@gmail.com>
2024-02-27 14:36:24 +0530

    Random commit

--- Client/Health/Clubbing - Claims.py
+++ Client/Health/Clubbing - Claims.py
@@ -4,27 +4,27 @@ import duckdb as db
 
 def prepare_claim_file():
     q3 = """select count(claim_num) as claim_count,sum(Aggregate) as Aggregate,
-            Zone,Mem_Gender_New,Renewal_Count_New,Financial_Year,
+            Zone_New,Mem_Gender_New,Renewal_Count_New,Financial_Year,
             Sum_Insured_New, Mem_Age_New, Product_Name_New,  
             Channel_type_New, Revised_Individual_Floater_New 
             from df            
-            group by Zone , Mem_Gender_New , Sum_Insured_New, Mem_Age_New, Product_name_New,Channel_type_New, 
+            group by Zone_New , Mem_Gender_New , Sum_Insured_New, Mem_Age_New, Product_name_New,Channel_type_New, 
             Revised_Individual_Floater_New,Financial_Year,Renewal_Count_New            
      """
 
     output = db.execute(q3).df()
-    output.to_csv("CSV\\ClaimsModelFile.csv")
+    output.to_csv("CSV\\ClaimsFile.csv")
 
 
 def group_renewal_count(x):
     if x in [0, 1]:
-        return "0 & 1"
+        return x
     if x in [2, 3]:
         return "2 & 3"
-    elif x in [4, 5, 6, 7]:
-        return "4 to 7"
-    elif x in [8, 9, 10, 11, 12]:
-        return x
+    elif x in [4, 5]:
+        return "4 &5"
+    elif x in [6, 7, 8]:
+        return "6 to 8"
 
 
 def group_gender(x):
@@ -40,56 +40,97 @@ def group_rif(x):
     if x in ["FLOATER", "INDIVIDUAL"]:
         return x
     else:
-        return "Other"
+        return "FLOATER"
 
 
 def group_channel_type(x):
-    if x in ["Agents", "Broker", "Office Direct", "Sales Direct", "Tele Marketer", "Bancassurance"]:
-        return x
+    if x in ["Agents", "Sales Direct"]:
+        return "Group 1"
+    elif x in ["Bancassurance", "Corporate Agents", "Pos Agents", "IMF", "NBFC-Bancassurance"]:
+        return "Group 2"
+    elif x in ["Tele Marketer", "Web Aggregator", "Broker"]:
+        return "Group 3"
     else:
-        return "Other"
+        return "Office Direct"
 
 
 def group_product_name(x):
-    if x in ["FHO", "COMP", "MCI", "SCRC", "Young Star Insurance Policy", "Star Health Assure Insurance Policy",
-             "SURPLUS-FLOATER", "SURPLUS-IND", "HEALTH GAIN", "Star Womens care Insurance Policy",
-             "Arogya Sanjeevini Policy"]:
+    if x in ["FHO", "COMP", "MCI"]:
         return x
+    elif x in ["SCRC", "Young Star Insurance Policy", "Arogya Sanjeevini Policy"]:
+        return "Group 1"
     else:
         return "Other"
 
 
 def group_si(x):
-    if x in [500000, 1000000, 300000, 400000, 1500000, 200000, 750000, 2500000, 2000000, 10000000, 5000000]:
+    if x in [100000, 150000, 200000, 300000, 400000, 500000, 750000, 1000000, 2500000]:
         return x
+    elif x in [1500000, 2000000, 10000000]:
+        return "Group 1"
     else:
         return "Other"
 
 
 def group_age(x):
-    if x in [78, 77, 76, 75, 74, 73, 72, 71, 70, 69]:
-        return "69 to 78"
-    elif x in [72, 71, 70, 69]:
-        return "69 to 72"
-    elif x in [61, 60, 59, 58, 57]:
-        return "57 to 61"
-    elif x in [31, 32, 33]:
-        return "31 to 33"
-    elif x in [30, 29, 28, 27]:
-        return "27 to 30"
-    elif x in [18, 19, 20, 21, 22, 23, 24]:
-        return "18 to 24"
-    else:
+    if x in [42, 43, 44, 45, 46, 66]:
+        return "Group 1"
+    elif x in [47, 48, 64, 65]:
+        return "Group 3"
+    elif x in [40, 41, 67]:
+        return "Group 5"
+    elif x in [49, 50, 51, 52, 53, 63]:
+        return "Group 6"
+    elif x in [38, 39]:
+        return "Group 7"
+    elif x in [17, 23, 24, 25, 34, 35, 36, 37]:
+        return "Group 8"
+    elif 54 <= x <= 62:
+        return "Group 9"
+    elif 69 <= x <= 88:
+        return "Group 10"
+    elif x in [14, 15, 16, 26, 27, 28, 30, 31, 32, 33, 90]:
+        return "Group 10"
+    elif x in [0, 1]:
+        return "Group 11"
+    elif x in [29]:
         return x
+    elif x in [2, 3]:
+        return "Group 13"
+    elif x in [18, 19, 20, 21, 22, 68]:
+        return "Group 14"
+    elif x in [4, 5]:
+        return "Group 15"
+    elif x in [6, 7, 8]:
+        return "Group 16"
+    elif x in [9, 10, 11]:
+        return "Group 17"
+    elif x in [12, 13]:
+        return "Group 18"
 
+
+zone_dict = {"DEL AO-II": "Zone 3", "MUMBAI": "Zone 1", "DEL AO-I": "Zone 3", "AHMEDABAD": "Zone 8",
+             "KERALA-SOUTH": "Zone 6",
+             "BANGALORE": "Zone 3", "CHANDIGARH": "Zone 5", "CHENNAI": "Zone 1", "DEHRADUN": "Zone 6",
+             "PUNE": "PUNE", "HYDERABAD": "Zone 4", "TIRUPATHI": "Zone 4", "KERALA-CENTRAL": "Zone 5",
+             "LUCKNOW": "Zone 7",
+             "LUDHIANA": "LUDHIANA", "INDORE": "Zone 8", "KERALA-NORTH": "Zone 5",
+             "KOLKATA": "Zone 3", "COIMBATORE": "Zone 8", "JAIPUR": "JAIPUR", "MADURAI": "Zone 7", "SALEM": "Zone 7",
+             "TRICHY": "TRICHY",
+             "PATNA": "Zone 11", "NAGPUR": "Zone 11", "WEB-SALES ONLINE": "Zone 3",
+             "RANCHI": "Zone 11", "GUWAHATI": "Zone 4", "ODISHA": "Zone 1", "CORPORATE OFFICE": "Zone 5"}
+
+FY_dict = {"FY18": 0, "FY19": 1, "FY20": 2, "FY21": 3, "FY22": 4, "FY23": 5}
 
 df = pd.read_csv("CSV\\PolicyMemberClaim.csv")
 
-df["Renewal_Count_New"] = df["Renewal_Count"].apply(lambda x: "Other" if x >= 13 else group_renewal_count(x))
-df["Mem_Age_New"] = df["Mem_Age"].apply(lambda x: "Other" if x >= 79 else group_age(x))
+df["Renewal_Count_New"] = df["Renewal_Count"].apply(lambda x: "Other" if x > 8 else group_renewal_count(x))
+df["Mem_Age_New"] = df["Mem_Age"].apply(lambda x: group_age(x))
 df["Sum_Insured_New"] = df["Sum_Insured"].apply(lambda x: group_si(x))
 df["Mem_Gender_New"] = df["Mem_Gender"].apply(lambda x: group_gender(x))
 df["Revised_Individual_Floater_New"] = df["Revised_Individual_Floater"].apply(lambda x: group_rif(x))
 df["Channel_type_New"] = df["Channel_type"].apply(lambda x: group_channel_type(x))
 df["Product_Name_New"] = df["policy_product_name"].apply(lambda x: group_product_name(x))
+df["Zone_New"] = df["Zone"].apply(lambda x: zone_dict[x])
+df["Financial_Year"] = df["Financial_Year"].apply(lambda x: FY_dict[x])
 prepare_claim_file()
--- Client/Health/Clubbing.py
+++ Client/Health/Clubbing.py
@@ -5,16 +5,16 @@ import duckdb as db
 def prepare_frequency_file():
     q3 = """select sum(EARNED_PREMIUM) as EARNED_PREMIUM,sum(PAID_AMT) as PAID_AMT,sum(Claim_Count) as Claim_Count, 
            sum(POLICIES_EXPOSED) as POLICIES_EXPOSED, sum(LIVES_EXPOSED) as LIVES_EXPOSED,
-            Zone_New,Mem_Gender_New,Renewal_Count_New,Financial_Year,
-            Sum_Insured_New, Mem_Age_New, Channel_type_New, Product_Name_New,Revised_Individual_Floater_New 
+            Zone_Frequency,Mem_Gender_Frequency,Renewal_Count_Frequency,Financial_Year,
+            Sum_Insured_Frequency, Mem_Age_Frequency, Channel_type_Frequency, Product_Name_Frequency,Revised_Individual_Floater_Frequency 
             from df            
-            group by Zone_New , Mem_Gender_New , Sum_Insured_New, Mem_Age_New, Channel_type_New, 
-            Product_Name_New,Financial_Year,Renewal_Count_New,Revised_Individual_Floater_New            
+            group by Zone_Frequency , Mem_Gender_Frequency , Sum_Insured_Frequency, Mem_Age_Frequency, Channel_type_Frequency, 
+            Product_Name_Frequency,Financial_Year,Renewal_Count_Frequency,Revised_Individual_Floater_Frequency            
      """
 
     output = db.execute(q3).df()
 
-    output.to_csv("CSV\\FrequencyModelFile.csv")
+    output.to_csv("CSV\\FrequencyFile.csv")
 
 
 def group_renewal_count(x):
@@ -134,15 +134,15 @@ zone_dict = {"DEL AO-II": "Zone 2", "MUMBAI": "Zone 3", "DEL AO-I": "Zone 3", "A
              "RANCHI": "Zone 7", "GUWAHATI": "Zone 8", "ODISHA": "Zone 8", "CORPORATE OFFICE": "Zone 5"}
 
 FY_dict = {"FY18": 0, "FY19": 1, "FY20": 2, "FY21": 3, "FY22": 4, "FY23": 5}
-df = pd.read_csv("CSV\\SummaryExposed_Merged.csv")
-df["Revised_Individual_Floater_New"] = df["Revised_Individual_Floater"].apply(lambda x: group_rif(x))
-df["Renewal_Count_New"] = df["Renewal_Count"].apply(lambda x: "Above 8" if x > 8 else group_renewal_count(x))
-df["Mem_Age_New"] = df["Mem_Age"].apply(lambda x: "Others" if x > 66 else group_age(x))
-df["Sum_Insured_New"] = df["Sum_Insured"].apply(lambda x: group_si(x))
-df["Mem_Gender_New"] = df["Mem_Gender"].apply(lambda x: group_gender(x))
-df["Channel_type_New"] = df["Channel_type"].apply(lambda x: group_channel_type(x))
-df["Zone_New"] = df["Zone"].apply(lambda x: zone_dict[x])
-df["Financial_Year"] = df["Financial_Year"].apply(lambda x: FY_dict[x])
-df["Product_Name_New"] = df["Product_name"].apply(lambda x:group_product_name(x))
-df.to_csv("Output\\clubbed_file.csv")
-prepare_frequency_file()
+# df = pd.read_csv("CSV\\SummaryExposed_Merged.csv")
+# df["Revised_Individual_Floater_Frequency"] = df["Revised_Individual_Floater"].apply(lambda x: group_rif(x))
+# df["Renewal_Count_Frequency"] = df["Renewal_Count"].apply(lambda x: "Above 8" if x > 8 else group_renewal_count(x))
+# df["Mem_Age_Frequency"] = df["Mem_Age"].apply(lambda x: "Others" if x > 66 else group_age(x))
+# df["Sum_Insured_Frequency"] = df["Sum_Insured"].apply(lambda x: group_si(x))
+# df["Mem_Gender_Frequency"] = df["Mem_Gender"].apply(lambda x: group_gender(x))
+# df["Channel_type_Frequency"] = df["Channel_type"].apply(lambda x: group_channel_type(x))
+# df["Zone_Frequency"] = df["Zone"].apply(lambda x: zone_dict[x])
+# df["Financial_Year"] = df["Financial_Year"].apply(lambda x: FY_dict[x])
+# df["Product_Name_Frequency"] = df["Product_name"].apply(lambda x:group_product_name(x))
+# # df.to_csv("Output\\clubbed_file.csv")
+# prepare_frequency_file()
