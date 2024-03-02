1c97aa5547147b411e44fd2210e5f60a4d51a327
s-jagannathan44 <jagannathan.s@gmail.com>
2024-03-02 10:54:37 +0530

    Finally Final

--- Client/Health/Tweedie.py
+++ Client/Health/Tweedie.py
@@ -12,7 +12,7 @@ def build_model(power, iter_):
     global df_train, df_test
 
     interaction = make_pipeline(
-        OneHotEncoder(drop=["Group 3", "MALE", "FHOFLOATER", "0.0", "Zone 1", "500000.0"]),
+        OneHotEncoder(drop=["Group 3", "MALE", "FHOFLOATER", "0.0", "Zone 8", "Group 1"]),
         PolynomialFeatures(degree=3, interaction_only=True, include_bias=False)
     )
 
@@ -43,7 +43,7 @@ def make_pivots(dataframe, columns):
     df2 = pd.pivot_table(dataframe, values="PAID_AMT  LIVES_EXPOSED".split(), columns=columns,
                          aggfunc="sum").T
     df2["Actual"] = df2["PAID_AMT"] / df2["LIVES_EXPOSED"]
-    df2.to_csv("Output\\" + columns + ".csv")
+    df2.to_csv("Output\\Sep\\" + columns + ".csv")
 
 
 def make_multi(dataframe, columns):
@@ -114,18 +114,14 @@ def aic(y, X_, pr, p):
 
 df = pd.read_csv("CSV\\TweedieModelFile_AgeModified.csv", usecols="Mem_Age_New Mem_Gender_New LIVES_EXPOSED "
                                                                   "PAID_AMT Financial_Year Renewal_Count_New Zone_New "
-                                                                  "Revised_Product_Name_New Channel_type_New Sum_Insured_New "
+                                                                  "Revised_Product_Name_New Channel_type_New "
+                                                                  "Sum_Insured_New "
                  .split())
 for col in df.columns:
     if "Unnamed" in col:
         df.drop(col, axis=1, inplace=True)
 
 df = df[df["LIVES_EXPOSED"] >= 1]
-df = df[~ df["Revised_Product_Name_New"].isin("SURPLUS-FLOATERFLOATER SURPLUS-FLOATERMED-PLT-046 "
-                                              "SURPLUS-INDINDIVIDUAL Corona Kavach PolicyFLOATER "
-                                              "Corona Kavach PolicyINDIVIDUAL"
-                                              " Star Hospital Cash Insurance PolicyFLOATER ".split())]
-df = df[df["Financial_Year"].isin([0, 1, 2, 4, 5])]
 df["Loss_Cost"] = df["PAID_AMT"] / df["LIVES_EXPOSED"]
 df["Loss_Cost"].fillna(0, inplace=True)
 df["Mem_Age_New"].fillna("0.0", inplace=True)
@@ -135,8 +131,8 @@ df_train, df_test = train_test_split(df, test_size=0.2, random_state=0)
 df_model = df_train[['Mem_Age_New', "Mem_Gender_New", "Revised_Product_Name_New", "Financial_Year", "Renewal_Count_New",
                      "Zone_New", "Sum_Insured_New"]]
 
-powers = [1.7]
-iterations = [5000]
+powers = [1.6]
+iterations = [45000]
 for p_ in powers:
     for i in iterations:
         print(p_, i)
@@ -148,17 +144,17 @@ def othering(dataframe):
     make_pivots(dataframe, "Revised_Product_Name_New")
     make_pivots(dataframe, "Zone_New")
     make_pivots(dataframe, "Mem_Age_New")
-    # make_pivots(dataframe, "Mem_Gender_New")
+    # make_pivots(dataframe, "Mem_Gender")
     make_pivots(dataframe, "Renewal_Count_New")
-    # make_pivots(dataframe, "Channel_type_New")
+    # make_pivots(dataframe, "Channel_type")
     make_pivots(dataframe, "Sum_Insured_New")
     make_pivots(dataframe, "Financial_Year")
 
 
 def find_separation():
-    df_ = pd.read_csv("CSV\\TweedieModelFile.csv", usecols="Mem_Age_New Mem_Gender_New LIVES_EXPOSED "
-                                                           "PAID_AMT Financial_Year Renewal_Count_New Zone_New "
-                                                           "Revised_Product_Name_New Channel_type_New "
-                                                           "Sum_Insured_New ".split())
+    df_ = pd.read_csv("CSV\\TweedieModelFile_AgeModified.csv", usecols="Mem_Age_New Mem_Gender_New LIVES_EXPOSED "
+                                                                       "PAID_AMT Financial_Year Renewal_Count_New "
+                                                                       "Zone_New Revised_Product_Name_New "
+                                                                       "Sum_Insured_New ".split())
 
     othering(df_)
--- Client/Health/TweedieClubbing.py
+++ Client/Health/TweedieClubbing.py
@@ -22,10 +22,10 @@ def group_renewal_count(x):
         return x
     elif x in [2, 3]:
         return "Group 1"
-    elif x in [4, 5, 6, 7, 8]:
+    elif x in [4, 5, 6, 7]:
         return "Group 2"
-    # elif x in [9, 10]:
-    #     return "Group 3"
+    elif x in [8, 9]:
+        return "Group 3"
 
 
 def group_gender(x):
@@ -61,42 +61,70 @@ def group_product_name(x):
     #     return x
     if x in ["FHOFLOATER", "MCIINDIVIDUAL"]:
         return x
-    elif x in ["COMPFLOATER"]:
+    elif x in ["COMPFLOATER", "Young Star Insurance PolicyIndividual", "FHO ACC CAREFLOATER",
+               "Star Womens care Insurance PolicyINDIVIDUAL", "Star Health Assure Insurance PolicyINDIVIDUAL"]:
         return "FHOFLOATER"
+    elif x in ["Star Cancer Care Group INDIVIDUAL"]:
+        return "MCIINDIVIDUAL"
     elif x in ["SCRCINDIVIDUAL", "COMPINDIVIDUAL"]:
-        return "Individual Group1"
-    elif x in ["Arogya Sanjeevini PolicyINDIVIDUAL", "Star Health Assure Insurance PolicyINDIVIDUAL"]:
-        return "Other"
+        return "Individual Group 1"
     elif x in ["Young Star Insurance PolicyFLOATER", "Arogya Sanjeevini PolicyFLOATER",
-               "Star Health Assure Insurance PolicyFLOATER", "Star Micro Rural and Farmers CareFLOATER"]:
+               "Star Health Assure Insurance PolicyFLOATER", "Star Womens care Insurance PolicyFLOATER",
+               "MCI ACC CAREINDIVIDUAL", "FHO ACC CARE INDIVIDUAL"]:
         return "Floater Group1"
+    elif x in ["Star Micro Rural and Farmers CareFLOATER", "True Value Health InsuranceFLOATER",
+               "FIRST OPTIMAINDIVIDUAL", "POS Family Health OptimaFLOATER",
+               "Star Micro Rural and Farmers CareINDIVIDUAL", "MCI FAMILYFLOATER", "MCI POS INDVINDIVIDUAL",
+               "Star Special CareINDIVIDUAL"]:
+        return "Floater Group5"
+    elif x in ["DIABETESINDIVIDUAL", "CARDIACINDIVIDUAL",
+               "DIABETESFLOATER", "",
+               "Star Care Micro Insurance PolicyFLOATER", "Star Cardiac Care Insurance Policy - PlatinumINDIVIDUAL"
+               ]:
+        return "Floater Group6"
+    elif x in ["DIABETESINDIVIDUAL", "CARDIACINDIVIDUAL",
+               "DIABETESFLOATER", "",
+               "Star Care Micro Insurance PolicyFLOATER", "Star Cardiac Care Insurance Policy - PlatinumINDIVIDUAL"
+               ]:
+        return "Floater Group6"
+    elif x in ["Star Delite Insurance PolicyINDIVIDUAL", "Star Care Micro Insurance PolicyINDIVIDUAL",
+               "Star First ComprehensiveINDIVIDUAL", "Star Critical Illness Multipay Insurance PolicyINDIVIDUAL",
+               "Star Outpatientcare Insurance PolicyFLOATER", "Micro Insurance IndividualINDIVIDUAL",
+               "Star Outpatientcare Insurance PolicyFLOATER"
+               ]:
+        return "Floater Group4"
+
     else:
         return "Other"
 
 
 def group_si(x):
-    if x in [400000, 500000]:
-        return x
-    elif x in [100000, 300000]:
+    if x in [400000, 1000000]:
         return "Group 2"
-    elif x in [150000, 200000, 750000]:
-        return "Group 4"
-    elif x in [1000000, 1500000, 2000000, 2500000, 5000000, 10000000]:
+    elif x in [100000, 300000, 500000]:
+        return "Group 1"
+    elif x in [150000, 200000, 750000, 1500000, 2000000]:
+        return "Group 3"
+    elif x in [2500000, 10000000]:
         return "Group 3"
     else:
         return "Other"
 
 
 def group_age(x):
-    if x in [0]:
+    if x in [0, 55]:
         return x
-    elif 1 <= x <= 3:
+    elif 1 <= x <= 2:
         return "Group 1"
-    elif 4 <= x <= 5:
+    elif x in [3]:
+        return "Group 5"
+    elif x in [4]:
+        return "Group 4"
+    elif x in [5, 14]:
         return "Group 2"
     elif 6 <= x <= 13:
         return "Group 3"
-    elif 14 <= x <= 17:
+    elif 15 <= x <= 17:
         return "Group 4"
     elif 18 <= x <= 19:
         return "Group 5"
@@ -106,45 +134,70 @@ def group_age(x):
         return "Group 7"
     elif 34 <= x <= 37:
         return "Group 8"
-    elif 38 <= x <= 42:
+    elif 38 <= x <= 41:
         return "Group 9"
-    elif 43 <= x <= 45:
+    elif 42 <= x <= 45:
         return "Group 10"
-    elif 46 <= x <= 49:
+    elif 46 <= x <= 50:
         return "Group 11"
-    elif 50 <= x <= 54:
+    elif 51 <= x <= 54:
         return "Group 12"
-    elif 55 <= x <= 60:
+    elif 56 <= x <= 59:
         return "Group 13"
+    elif x in [60]:
+        return "Others"
     elif 61 <= x <= 66:
         return "Group 14"
 
 
-zone_dict = {"DEL AO-II": "Zone 1", "MUMBAI": "Zone 1", "DEL AO-I": "Zone 1", "AHMEDABAD": "Zone 2",
+def prepare_forecast_file():
+    df_24 = pd.read_csv("CSV\\SummaryExposed_24.csv")
+    df_24["Revised_Product_name"] = (
+            df_24["Product_name"] + df_24["Revised_Individual_Floater"].apply(lambda x: group_rif(x)))
+    df_24["Financial_Year"] = 6
+    df_24 = df_24[~ df_24["Product_name"].isin("SURPLUS-FLOATER SURPLUS-IND".split())]
+    df_24 = df_24[~ df_24["Product_name"].str.contains("Corona")]
+    df_24 = df_24[~ df_24["Product_name"].str.contains("Cash")]
+    df_24["Renewal_Count_New"] = df_24["Renewal_Count"].apply(lambda x: "Above 9" if x > 9 else group_renewal_count(x))
+    df_24["Mem_Age_New"] = df_24["Mem_Age"].apply(lambda x: "Others" if x > 66 else group_age(x))
+    df_24["Sum_Insured_New"] = df_24["Sum_Insured"].apply(lambda x: group_si(x))
+    df_24["Mem_Gender_New"] = df_24["Mem_Gender"].apply(lambda x: group_gender(x))
+    df_24["Channel_type_New"] = df_24["Channel_type"].apply(lambda x: group_channel_type(x))
+    df_24["Revised_Product_Name_New"] = df_24["Revised_Product_name"].apply(lambda x: group_product_name(x))
+    df_24["Zone_New"] = df_24["Zone"].apply(lambda x: zone_dict[x])
+
+    df_24.to_csv("Output\\24_File.csv")
+
+
+zone_dict = {"DEL AO-II": "DEL AO-II", "MUMBAI": "Zone 1", "DEL AO-I": "Zone 1", "AHMEDABAD": "AHMEDABAD",
              "KERALA-SOUTH": "Zone 2",
              "BANGALORE": "Zone 2", "CHANDIGARH": "Zone 2", "CHENNAI": "Zone 3", "DEHRADUN": "Zone 3",
-             "PUNE": "Zone 4", "HYDERABAD": "Zone 4", "TIRUPATHI": "Zone 4", "KERALA-CENTRAL": "Zone 5",
+             "PUNE": "Zone 4", "HYDERABAD": "Zone 3", "TIRUPATHI": "Zone 3", "KERALA-CENTRAL": "Zone 5",
              "LUCKNOW": "Zone 5",
-             "LUDHIANA": "Zone 5", "INDORE": "Zone 7", "KERALA-NORTH": "Zone 7",
-             "KOLKATA": "Zone 8", "COIMBATORE": "Zone 8", "JAIPUR": "Zone 8", "MADURAI": "Zone 9", "SALEM": "Zone 9",
-             "TRICHY": "Zone 9",
-             "PATNA": "Others", "NAGPUR": "Others", "WEB-SALES ONLINE": "Others",
+             "LUDHIANA": "Zone 5", "INDORE": "Zone 5", "KERALA-NORTH": "Zone 5",
+             "KOLKATA": "Zone 8", "COIMBATORE": "Zone 8", "JAIPUR": "Zone 8", "MADURAI": "Zone 8", "SALEM": "Zone 8",
+             "TRICHY": "Zone 8",
+             "PATNA": "Zone 8", "NAGPUR": "Zone 8", "WEB-SALES ONLINE": "Zone 8",
              "RANCHI": "Others", "GUWAHATI": "Others", "ODISHA": "Others", "CORPORATE OFFICE": "Others"}
 
-FY_dict = {"FY18": 0, "FY19": 1, "FY20": 2, "FY21": 3, "FY22": 4, "FY23": 5}
-df = pd.read_csv("CSV\\SummaryExposed_24.csv")
-# df = df[~ df["ICD_category"].isin(["Certain conditions originating in the perinatal period",
-#                                    "Congenital malformations, deformations and chromosomal abnormalities",
-#                                    "Mental, Behavioral and Neurodevelopmental disorders"])]
-
-df["Revised_Product_name"] = df["Product_name"] + df["Revised_Individual_Floater"].apply(lambda x: group_rif(x))
-df["Renewal_Count_New"] = df["Renewal_Count"].apply(lambda x: "Above 8" if x > 8 else group_renewal_count(x))
+FY_dict = {"FY18": 0, "FY19": 1, "FY20": 2, "FY21": 3, "FY22": 4, "FY23": 5, "Test": 6}
+# df = pd.read_csv("CSV\\SummaryExposed_Merged.csv")
+# df["Revised_Product_name"] = df["Product_name"] + df["Revised_Individual_Floater"].apply(lambda x: group_rif(x))
+# df["Financial_Year"] = df["Financial_Year"].apply(lambda x: FY_dict[x])
+# df = df[~ df["Product_name"].isin("SURPLUS-FLOATER SURPLUS-IND"
+#                                   .split())]
+# df = df[ ~ df["Product_name"].str.contains("Corona")]
+# df = df[ ~ df["Product_name"].str.contains("Cash")]
+# df = df[df["Financial_Year"].isin([0, 1, 2, 4, 5])]
+# df.to_csv("Output\\SummaryFile.csv")
+df = pd.read_csv("Output\\SummaryFile.csv")
+df["Renewal_Count_New"] = df["Renewal_Count"].apply(lambda x: "Above 9" if x > 9 else group_renewal_count(x))
 df["Mem_Age_New"] = df["Mem_Age"].apply(lambda x: "Others" if x > 66 else group_age(x))
 df["Sum_Insured_New"] = df["Sum_Insured"].apply(lambda x: group_si(x))
 df["Mem_Gender_New"] = df["Mem_Gender"].apply(lambda x: group_gender(x))
 df["Channel_type_New"] = df["Channel_type"].apply(lambda x: group_channel_type(x))
 df["Revised_Product_Name_New"] = df["Revised_Product_name"].apply(lambda x: group_product_name(x))
 df["Zone_New"] = df["Zone"].apply(lambda x: zone_dict[x])
-df["Financial_Year"] = df["Financial_Year"].apply(lambda x: FY_dict[x])
+
 df.to_csv("Output\\clubbed_file_AgeModified.csv")
 prepare_tweedie_file()
