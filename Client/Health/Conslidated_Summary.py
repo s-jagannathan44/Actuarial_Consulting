ab8ba734944cb6938e3ef017a28687d2d27fa778
s-jagannathan44 <jagannathan.s@gmail.com>
2024-03-25 17:29:27 +0530

    FY24 summary

--- Client/Health/Conslidated_Summary.py
+++ Client/Health/Conslidated_Summary.py
@@ -19,22 +19,18 @@ policy_claim.Policy_number = member.policy_number and
 
 
 def summaries():
-    q3 = """select sum(Normalized_Earned_Premium) as EARNED_PREMIUM,sum(Aggregate) as PAID_AMT,sum(claim_count) as Claim_Count, 
-           sum(Normalized_POLICIES_EXPOSED) as POLICIES_EXPOSED, sum(Normalized_LIVES_EXPOSED) as LIVES_EXPOSED,
-            sum(members_per_policy) as MEMBER_COUNT, 
-            icd_category,Zone,Mem_Gender,Renewal_Count,norm_policy.Financial_Year,
-            norm_policy.Sum_Insured, member_claim.Mem_Age, Product_name,  
+    q3 = """select sum(Normalized_Earned_Premium) as EARNED_PREMIUM, 
+           sum(Normalized_POLICIES_EXPOSED) as POLICIES_EXPOSED, sum(Normalized_LIVES_EXPOSED) as LIVES_EXPOSED,             
+            norm_policy.Zone,Mem_Gender,Renewal_Count,
+            norm_policy.Sum_Insured, Mem_Age, Product_name,  
             Channel_type, Revised_Individual_Floater 
             from norm_policy
-            inner join member_claim on
-            norm_policy.Policy_number = member_claim.Policy_number  
-            where member_claim.Mem_ID !=''
-            group by norm_policy.Zone , member_claim.Mem_Gender , norm_policy.Sum_Insured, member_claim.Mem_Age, 
-            Product_name,Channel_type, Revised_Individual_Floater,icd_category,norm_policy.Financial_Year,Renewal_Count            
+            group by norm_policy.Zone , Mem_Gender , norm_policy.Sum_Insured, Mem_Age, 
+            Product_name,Channel_type, Revised_Individual_Floater,Renewal_Count            
      """
 
     output = db.execute(q3).df()
-    output.to_csv("CSV\\SummaryExposed_Merged.csv")
+    output.to_csv("CSV\\SummaryExposed_24..csv")
 
 
 def rename_columns(file_name):
@@ -45,7 +41,7 @@ def rename_columns(file_name):
                               'Previous Company Name', 'Inception Date', 'Renewal Count', 'NOP_RCD', 'NOR_RCD',
                               'GWP_RCD',
                               'EARNED_PREMIUM', 'POLICIES_EXPOSED', 'LIVES_EXPOSED', 'Revised Individual/Floater',
-                              'POLICY_VERSION_CODE', 'Sum Insured'])
+                              'Sum Insured'])
     dx.rename(columns={'Policy number': "Policy_number", 'Policy Start Date': "Policy_Start_Date",
                        'Policy End Date': 'Policy_End-Date',
                        'Product Name': 'Product_name', 'Plan type': 'Plan_type', 'Policy Period': 'Policy_Period',
@@ -60,25 +56,25 @@ def rename_columns(file_name):
 
 
 def merge_policies():
-    path = "C:\\SHAI\\Revised 11-12-23\\Data\\*.csv"
+    path = "C:\\SHAI\\Revised 11-12-23\\Data\\TestData\\FY24 upto Jan'24 Exposure Mapped_Final_v1.csv"
     concatenated_policies = pd.DataFrame()
     files = glob.glob(path)
     for file_name in files:
-        financial_year = file_name[30:34]
+        financial_year = file_name[39:43]
         frame = rename_columns(file_name)
         frame["Financial_Year"] = financial_year
         frame["Policy_number"] = frame["Policy_number"].apply(lambda x: financial_year + x)
         concatenated_policies = pd.concat([concatenated_policies, frame], axis=0)
         print(financial_year)
-    concatenated_policies.to_csv("CSV\\Policy_Merged.csv")
+    concatenated_policies.to_csv("CSV\\Policy_24.csv")
 
 
 def merge_members():
-    path = "C:\\SHAI\\Revised 11-12-23\\Data\\*.csv"
+    path = "C:\\SHAI\\Revised 11-12-23\\Data\\TestData\\FY24 upto Jan'24 Exposure Mapped_Final_v1.csv"
     concatenated_members = pd.DataFrame()
     files = glob.glob(path)
     for file_name in files:
-        financial_year = file_name[30:34]
+        financial_year = file_name[39:43]
         frame = pd.read_csv(file_name, usecols=['Member ID 1', 'Member ID 2', 'Member ID 3', 'Member ID 4',
                                                 'Member ID 5',
                                                 'Member ID 6', 'Member Age 1', 'Member Age 2', 'Member Age 3',
@@ -96,7 +92,7 @@ def merge_members():
         print(financial_year)
         concatenated_members = pd.concat([concatenated_members, final_member], axis=0)
 
-    concatenated_members.to_csv("CSV\\Member_Merged.csv")
+    concatenated_members.to_csv("CSV\\Member_24.csv")
 
 
 def transform_member(member):
@@ -191,14 +187,14 @@ def remove_group(claims, is_os):
 # merge_claim()
 # print("claims  have been merged")
 
-claim = pd.read_csv("CSV\\Claims_Merged.csv")
-policy_file = pd.read_csv("CSV\\Policy_Merged.csv")
-member = pd.read_csv("CSV\\Member_Merged.csv")
+# claim = pd.read_csv("CSV\\Claims_Merged.csv")
+policy = pd.read_csv("CSV\\Policy_24.csv")
+member = pd.read_csv("CSV\\Member_24.csv")
 print("files have been loaded")
-member_per_policy_count = db.sql(
-    """ select Policy_number, count(Mem_ID) as members_per_policy  from member group by Policy_number """).df()
-policy = policy_file.merge(member_per_policy_count, on="Policy_number")
-member_claim = member.merge(claim, on=["Policy_number", "Mem_ID", "Financial_Year"], how="left")
+# member_per_policy_count = db.sql(
+#     """ select Policy_number, count(Mem_ID) as members_per_policy  from member group by Policy_number """).df()
+# policy = policy_file.merge(member_per_policy_count, on="Policy_number")
+member_claim = policy.merge(member, on=["Policy_number"], how="left")
 member_count = db.sql(
     """ select Policy_number, count(Mem_ID) as count  from member_claim group by Policy_number """).df()
 
@@ -207,8 +203,9 @@ q2 = """select policy.Policy_number, EARNED_PREMIUM/count  as Normalized_Earned_
         from policy join member_count
         on policy.Policy_number =member_count.Policy_number"""
 nep = db.execute(q2).df()
-norm_policy = policy.merge(nep, on="Policy_number")
+norm_policy = member_claim.merge(nep, on="Policy_number")
+
 print("entering summaries")
 summaries()
-print("Creating Severity FIle ")
-create_file()
+# print("Creating Severity FIle ")
+# create_file()
