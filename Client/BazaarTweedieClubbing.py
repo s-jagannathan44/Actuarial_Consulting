b66d725d97a53fccb2434e5a54b62e6fe3bb3a54
s-jagannathan44 <jagannathan.s@gmail.com>
2024-04-04 09:41:35 +0530

    Final 4 Wheeler clubbing linked to Analysis V10 and 4 Wheeler model data xls

--- Client/BazaarTweedieClubbing.py
+++ Client/BazaarTweedieClubbing.py
@@ -4,11 +4,11 @@ import duckdb as db
 
 def prepare_tweedie_file():
     q3 = """select  Zone_new, cc_New, Accident_Year_new, 
-           Insurer_new, plancategory_new, roundage_new, makename_new, fuel_new,
-           sum(EP) as EARNED_PREMIUM,sum(PAID_AMT) as PAID_AMT,sum(Claim_Count) as Claim_Count, 
+           Insurer_Fuel_new, plancategory_new, roundage_new, makename_new,
+           sum(PAID_AMT) as PAID_AMT,sum(Claim_Count) as Claim_Count, 
            sum(LIVES_EXPOSED) as LIVES_EXPOSED                       
             from df            
-            group by  Insurer_new, plancategory_new, roundage_new, makename_new, fuel_new, Accident_Year_new, 
+            group by  Insurer_Fuel_new, plancategory_new, roundage_new, makename_new,  Accident_Year_new, 
                       Zone_new, cc_New           
      """
 
@@ -17,21 +17,23 @@ def prepare_tweedie_file():
     output.to_csv("Bazaar\\Output\\4WheeleerFiles.csv")
 
 
-def group_insurer(x):
-    if x in ["Oriental Pvt Car Comp", "FG Pvt Car Comp+SATP"]:
+def group_insurer_fuel(x):
+    if x in ["National Pvt Car SATP_Petrol"]:
         return x
-    if x in ["National Pvt Car Comp", "Zuno_Pvt_Car_COMP_SATP"]:
-        return "Group 2"
-    elif x in ["Bajaj Pvt Car Comp", "Liberty Pvt Car COMP+SA", "Universal Sompo Pvt Car Comp+SATP"]:
-        return "Group 3"
-    elif x in ["Bajaj Pvt Car SATP", "Shriram Pvt Car Comp+ SATP"]:
-        return "Group 4"
-    elif x in ["RSA Pvt Car COMP+SATP", "KGI SATP+COMP Pvt Car"]:
+    if x in ["Chola Pvt Car Comp+SATP_Diesel", "Oriental Pvt Car Comp_Diesel"]:
         return "Group 1"
-    elif x in ["SBI Pvt Car Comp+SATP", "Oriental Pvt Car SATP", "Chola Pvt Car Comp+SATP"]:
+    if x in ["National Pvt Car SATP_Diesel", "Oriental Pvt Car Comp_Petrol", "SBI Pvt Car Comp+SATP_Petrol"]:
+        return "Group 2"
+    elif x in ["Bajaj Pvt Car SATP_Petrol", "Oriental Pvt Car SATP_Petrol", "Shriram Pvt Car Comp+ SATP_Diesel", "Shriram Pvt Car Comp+ SATP_Petrol"]:
+        return "Group 3"
+    elif x in ["Bajaj Pvt Car Comp_Petrol", "Chola Pvt Car Comp+SATP_Petrol", "SBI Pvt Car Comp+SATP_CNG"]:
+        return "Group 4"
+    elif x in ["FG Pvt Car Comp+SATP_Diesel", "National Pvt Car Comp_Diesel", "Universal Sompo Pvt Car Comp+SATP_Petrol"]:
         return "Group 6"
+    elif x in ["FG Pvt Car Comp+SATP_Petrol", "National Pvt Car Comp_Petrol"]:
+        return "Group 7"
     else:
-        return "1National Pvt Car SATP"
+        return "1Others"
 
 
 def group_plancategory(x):
@@ -42,40 +44,30 @@ def group_plancategory(x):
 
 
 def group_roumdage(x):
-    if x in [9]:
-        return x
-    elif x in [0, 4, 20]:
-        return "Group 1"
-    elif x in [1, 11]:
+    if x in [3, 4]:
+        return "G"+str(x)
+    elif x in [9, 11]:
         return "Group 2"
-    elif x in [3, 7, 12, 19]:
-        return "1Group 3"
-    elif x in [5, 8, 13]:
+    elif x in [7, 8, 19]:
         return "Group 4"
-    elif x in [6, 10]:
+    elif x in [2, 5]:
         return "Group 5"
+    elif x in [6, 10, 18]:
+        return "Group 6"
     else:
-        return "Others"
+        return "1Others"
 
 
 def group_makename(x):
-    if x in ["HONDA", "RENAULT"]:
-        return "Group 1"
+    if x in ["HONDA", "TATA"]:
+        return "Group 3"
     elif x in ["HYUNDAI"]:
         return x
     elif x in ["MARUTI"]:
         return "1MARUTI"
-    elif x in ["TATA", "CHEVROLET"]:
-        return "Group 2"
     else:
         return "Others"
 
-
-def group_fuel(x):
-    if x in ["Diesel"]:
-        return "Diesel"
-    else:
-        return "1Petrol"
 
 
 def group_zone(x):
@@ -86,21 +78,19 @@ def group_zone(x):
 
 
 def group_cc(x):
-    if x in ["Below 1000cc", "1000 to 1500cc"]:
-        return "1000 to 1500cc"
+    if x in ["Above 1500cc", "1000 to 1500cc"]:
+        return "1Group 1"
     else:
         return x
 
 
 def group_AY(x):
-    if x in [2018, 2019, 2020]:
-        return 1
     if x in [2021]:
-        return 2
+        return 1
     elif x in [2022]:
-        return 3
+        return 2
     elif x in [2023]:
-        return 4
+        return 3
 
 
 def make_pivots(dataframe, columns):
@@ -111,10 +101,9 @@ def make_pivots(dataframe, columns):
 
 
 def othering(dataframe):
-    make_pivots(dataframe, "Insurer_new")
+    make_pivots(dataframe, "Insurer_Fuel_new")
     make_pivots(dataframe, "Zone_new")
     make_pivots(dataframe, "cc_new")
-    make_pivots(dataframe, "fuel_new")
     make_pivots(dataframe, "plancategory_new")
     make_pivots(dataframe, "roundage_new")
     make_pivots(dataframe, "makename_new")
@@ -126,26 +115,17 @@ def find_separation():
     othering(df_)
 
 
-def prepare_large_loss():
-    df2 = df[df["Insurer"].str.contains("Revised Pvt Car COMP S")]
-    df3 = df[df["Zone_1"].isna()]
-    df4 = df[df["PAID_AMT"] > 10000000]
-    large_loss = pd.concat([df2, df3, df4])
-    large_loss.to_csv("Bazaar\\Output\\exclusions.csv")
-
-
 df = pd.read_csv("4Wheeler.csv")
 df["roundage"] = df["roundage"].apply(pd.to_numeric, errors="coerce")
 df = df[~ df["Insurer"].str.contains("Revised Pvt Car COMP S")]
 df["PAID_AMT"].fillna(0, inplace=True)
 df.dropna(subset=["Zone_1"], inplace=True)
-df["Insurer_new"] = df["Insurer"].apply(lambda x: group_insurer(x))
+df["Insurer_Fuel_new"] = df["Insurer_Fuel"].apply(lambda x: group_insurer_fuel(x))
 df["Zone_new"] = df["Zone_1"].apply(lambda x: group_zone(x))
 df["plancategory_new"] = df["newplancategory"].apply(lambda x: group_plancategory(x))
 df["roundage_new"] = df["roundage"].apply(lambda x: group_roumdage(x))
 df["cc_new"] = df["cubiccapacity_New"].apply(lambda x: group_cc(x))
 df["makename_new"] = df["makename"].apply(lambda x: group_makename(x))
-df["fuel_new"] = df["fuel"].apply(lambda x: group_fuel(x))
 df["roundage_new"].fillna("Others", inplace=True)
 df["Accident_Year_new"] = df["Accident_Year"].apply(lambda x: group_AY(x))
 df.to_csv("Bazaar\\Output\\4WheelerTestFile.csv")
--- Client/Bazaar_GLM.py
+++ Client/Bazaar_GLM.py
@@ -5,17 +5,42 @@ from statsmodels.genmod.families import Tweedie
 import statsmodels.formula.api as smf
 
 
+def make_multi(dataframe, columns):
+    df2 = pd.pivot_table(dataframe, values="PAID_AMT Pred_Cost LIVES_EXPOSED".split(), columns=columns,
+                         aggfunc="sum").T
+    df2["Actual"] = df2["PAID_AMT"] / df2["LIVES_EXPOSED"]
+    df2["Predicted"] = df2["Pred_Cost"] / df2["LIVES_EXPOSED"]
+    df2["Error"] = (df2["Actual"] - df2["Predicted"]) / df2["Actual"]
+    df2["Error"] = df2["Error"].abs()
+    df2['%ageError'] = df2['Error'].map('{:.2%}'.format)
+    below_ten = df2[df2["Error"] <= 0.1]["LIVES_EXPOSED"].sum()
+    total = df2["LIVES_EXPOSED"].sum()
+    print('{:.2%}'.format(below_ten / total))
+    df2.to_csv("Bazaar\\Output\\multi.csv")
+
+
+def make_pivots(dataframe, columns):
+    df2 = pd.pivot_table(dataframe, values="PAID_AMT  Pred_Cost LIVES_EXPOSED".split(), columns=columns,
+                         aggfunc="sum").T
+    df2["Actual"] = df2["PAID_AMT"] / df2["LIVES_EXPOSED"]
+    df2["Predicted"] = df2["Pred_Cost"] / df2["LIVES_EXPOSED"]
+    df2["Error"] = (df2["Actual"] - df2["Predicted"]) / df2["Actual"]
+    df2["Error"] = df2["Error"].abs()
+    df2['%ageError'] = df2['Error'].map('{:.2%}'.format)
+    df2.to_csv("Bazaar\\Output\\" + columns + ".csv")
+
+
 def build_model():
     df = pd.read_csv("Bazaar\\Output\\4WheeleerFiles.csv")
     df["Loss_Cost"] = df["PAID_AMT"] / df["LIVES_EXPOSED"]
     result = smf.glm(
-        formula='Loss_Cost ~ Accident_Year_new + Zone_new + cc_new + Insurer_new + plancategory_new + roundage_new  + makename_new + fuel_new',
+        formula='Loss_Cost ~ Accident_Year_new + Zone_new + cc_new + Insurer_Fuel_new + plancategory_new + roundage_new  + makename_new',
         data=df, family=Tweedie(var_power=1.9), var_weights=df["LIVES_EXPOSED"]).fit()
     joblib.dump(result, "Bazaar\\Output\\4Wheeleer.sav")
     return df
 
 
-df = pd.read_csv("Bazaar\\Output\\4WheelerTestFile.csv")   #build_model()
+df = pd.read_csv("Bazaar\\Output\\4WheelerTestFile.csv")  # build_model()
 result2 = joblib.load("Bazaar\\Output\\4Wheeleer.sav")
 print("-----------Summary-----------")
 print(result2.summary2())
@@ -26,3 +51,10 @@ y_pred = result2.predict(df)
 df["Pred"] = y_pred
 df["Pred_Cost"] = df["Pred"] * df["LIVES_EXPOSED"]
 df.to_csv("Bazaar\\Output\\new_4wheeler.csv")
+make_pivots(df, "Accident_Year")
+make_pivots(df, "Zone_1")
+make_pivots(df, "cubiccapacity_New")
+make_pivots(df, "newplancategory")
+make_pivots(df, "Insurer_Fuel")
+make_pivots(df, "makename")
+make_pivots(df, "roundage")
