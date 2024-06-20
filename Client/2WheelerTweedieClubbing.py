b0dd544dfc03b9a3b902a5e2c32ac0478b68a85a
s-jagannathan44 <jagannathan.s@gmail.com>
2024-06-20 10:37:49 +0530

    National commit

--- Client/2WheelerTweedieClubbing.py
+++ Client/2WheelerTweedieClubbing.py
@@ -4,11 +4,11 @@ import duckdb as db
 
 def prepare_tweedie_file():
     q3 = """select  
-           Accident_Year_new,Zone_new,cc_New,Make_new,Insurer_new, body_type_new, 
+           Accident_Year_new , cc_New, body_type_new,Zone_new,Make_new,Insurer_new,
            sum(PAID_AMT) as PAID_AMT,sum(Count) as Claim_Count, 
            sum(LIVES_EXPOSED) as LIVES_EXPOSED                       
             from df            
-            group by  Accident_Year_new,Zone_new,cc_New,Make_new,Insurer_new, body_type_new         
+            group by  Accident_Year_new , cc_New, body_type_new,Zone_new,Make_new,planType_new,Insurer_new 
      """
 
     output = db.execute(q3).df()
@@ -16,27 +16,8 @@ def prepare_tweedie_file():
     output.to_csv("Bazaar\\TW\\CSV\\Files\\Output\\2WheeleerNewFiles.csv")
 
 
-def group_age(x):
-    if x in [4, 10]:
-        return "G" + str(x)
-    elif x in [5, 3]:
-        return "Group 1"
-    elif x in [7, 13]:
-        return "Group 3"
-    elif x in [9, 6, 8]:
-        return "1Group 4"
-    elif x in [8, 11]:
-        return "Group 5"
-    elif x in [14, 12]:
-        return "Group 6"
-    elif x in [2, 16]:
-        return "Group 6"
-    else:
-        return "Others"
-
-
 def group_zone(x):
-    if x in ["North", "South", "East"]:
+    if x in ["North", "South"]:
         return "1North"
     else:
         return x
@@ -65,17 +46,14 @@ def group_cc(x):
         return "Others"
 
 
-def group_PlanType(x):
-    if x in ["TP"]:
-        return "1TP"
-    else:
-        return "COMP"
+def group_plan(x):
+    return x
 
 
 def group_make(x):
-    if x in ["HONDA", "Bajaj"]:
+    if x in ["HONDA", "HERO MOTOCORP", "Bajaj"]:
         return "1HONDA"
-    elif x in ["HERO MOTOCORP", "TVS", "HERO HONDA"]:
+    elif x in ["TVS", "HERO HONDA"]:
         return x
     else:
         return "Others"
@@ -85,7 +63,7 @@ def group_Insurer(x):
     if x in ["BAGIC 2W COMP+SATP Bookings", "United 2W Comp+SATP Bookings"]:
         return "1Group 3"
     elif x in ["ZUNO Comp+SATP Bookings 2", "SGI Comp+SATP Bookings 2", "Oriental SATP +COMP booking",
-               "NIA Comp+SATP Bookings"]:
+               "National 2W Comp+SATP Bookings","NIA Comp+SATP Bookings"]:
         return x
     else:
         return "Others"
@@ -113,13 +91,16 @@ def find_separation():
     othering(df_)
 
 
-df = pd.read_csv("2Wheeler_Inflated.csv")
+df = pd.read_csv("2Wheeler_Forecast.csv")
 df["Age"] = df["Age"].apply(pd.to_numeric, errors="coerce")
+df["PAID_AMT"].fillna(0, inplace=True)
 df["Zone_new"] = df["Zone"].apply(lambda x: group_zone(x))
+df["cc_new"] = df["ccnew"].apply(lambda x: group_cc(x))
 df["body_type_new"] = df["body_type"].apply(lambda x: group_body_type(x))
 df["Make_new"] = df["Make"].apply(lambda x: group_make(x))
-df["cc_new"] = df["ccnew"].apply(lambda x: group_cc(x))
+df["PlanType_new"] = df["plan_category"].apply(lambda x: group_plan(x))
 df["Insurer_new"] = df["Insurer"].apply(lambda x: group_Insurer(x))
 df["Accident_Year_new"] = df["Accident_Year"].apply(lambda x: group_AY(x))
-df.to_csv("Bazaar\\TW\\CSV\\Files\\Output\\2WheelerInflatedFile.csv")
-
+# prepare_tweedie_file()
+df.to_csv("Bazaar\\TW\\CSV\\Files\\Output\\2WheelerNewForecastFile.csv")
+# find_separation()
