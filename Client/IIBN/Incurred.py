8aa4aa8a2a162eb42e8329e64d1ea6d7d555df11
s-jagannathan44 <jagannathan.s@gmail.com>
2024-09-23 12:34:03 +0530

    Modifications suggested on 23 Sep to calculate count using  accident year& month instead of intimation month

--- Client/IIBN/Incurred.py
+++ Client/IIBN/Incurred.py
@@ -16,8 +16,10 @@ def print_row(row):
     return incurred_
 
 
-claims = pd.read_csv("Output\\RSA_Combined_Claims.csv")
+claims = pd.read_csv("Bazaar\\Bajaj_Claims Combine '23 to sep'24.csv")
+start_of_time = "01/03/2023"
 claims['Intimation_Date'] = pd.to_datetime(claims['Intimation_Date'], format="mixed", dayfirst=True)
+claims['Loss_Date'] = pd.to_datetime(claims['Loss_Date'], format="mixed", dayfirst=True)
 claims['File'] = pd.to_datetime(claims['File'], format="mixed", dayfirst=True)
 
 
@@ -25,17 +27,17 @@ list_claims = claims["Claim_Reference"].unique().tolist()
 print(len(list_claims))
 counter = 0
 for claim_reference in list_claims:
-    in_current_period = False
+    # in_current_period = False
     counter = counter + 1
     print(counter)
     claim_reference_ = claim_reference
     cr = claims[claims["Claim_Reference"] == claim_reference].sort_values(by=["File"])
     intimation_date = cr["Intimation_Date"].iloc[0]
-    start_of_time = "07/03/2023"
+
     if intimation_date >= datetime.strptime(start_of_time, "%d/%m/%Y"):
         opening_paid = 0
         opening_os = 0
-        in_current_period = True
+        # in_current_period = True
     else:
         opening_paid = int(cr["PaidClaimAmount"].iloc[0])
         opening_os = int(cr["Outstanding_Amount"].iloc[0])
@@ -45,10 +47,14 @@ for claim_reference in list_claims:
     for amount in incurred:
         index_ = incurred.index[count]
         claims.at[index_, "Incurred"] = amount
-        if count == 0 and in_current_period and cr["File"].iloc[0].month == intimation_date.month:
+        loss_date = claims.at[index_, "Loss_Date"]
+        intimation_date = claims.at[index_, "Intimation_Date"]
+        claims.at[index_, "Loss_Month"] = str(loss_date.month) + "_" + str(loss_date.year)
+        claims.at[index_, "Intimation_Month"] = str(intimation_date.month) + "_" + str(intimation_date.year)
+        if count == 0:
             claims.at[index_, "Claim_Count"] = 1
         else:
             claims.at[index_, "Claim_Count"] = 0
         count = count + 1
 
-claims.to_csv("Output\\RSA_Incurred_Claims.csv")
+claims.to_csv("Bazaar\\Output\\Bajaj_Incurred_Claims_v2.csv")
