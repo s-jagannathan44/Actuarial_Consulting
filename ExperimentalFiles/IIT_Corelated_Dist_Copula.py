575cda64559ab3848ed0958fb1c3b43efa23d1ff
s-jagannathan44 <jagannathan.s@gmail.com>
2024-10-21 14:21:37 +0530

    Final code

--- ExperimentalFiles/IIT_Corelated_Dist_Copula.py
+++ ExperimentalFiles/IIT_Corelated_Dist_Copula.py
@@ -1,20 +1,87 @@
 from matplotlib import pyplot as plt
 from scipy import stats
 import pandas as pd
+from matplotlib.ticker import PercentFormatter
 import seaborn as sns
-# import numpy as np
-from fitter import Fitter, get_common_distributions, get_distributions
+import numpy as np
 
-df = pd.read_csv("AustralianAuto.csv")
-a_plus_b = df["A_plus_B"]
-sns.histplot(a_plus_b)
-plt.show()
+# from fitter import Fitter, get_common_distributions, get_distributions
 
-df = df[df["ClaimNb"] > 0]
-# x1 = np.log(df["ClaimNb"])
-# x2 = np.log(df["KillInjNb"])
+df = pd.read_csv("AustralianAutoModified.csv")
+# df = df[df["ClaimNb"] > 0]
+# df.describe(percentiles=[0.25, 0.5, 0.75, .95]).to_csv("summary.csv")
+# df.var().to_csv("var.csv")
 x1 = df["ClaimNb"]
 x2 = df["KillInjNb"]
+a_plus_b = df["A_plus_B"]
+# plt.hist(x1,  alpha=0.5, label='x1',color='red')
+# plt.hist(x2,  alpha=0.5, label='x2', color='blue')
+# plt.hist(a_plus_b,  alpha=0.5, label='x1+x2', color='green')
+# plt.legend(loc='upper right')
+# plt.show()
+
+mvnorm = stats.multivariate_normal(mean=[0, 0], cov=[[1., 0.95],
+                                                     [0.95, 1.]])
+x = mvnorm.rvs(15000)
+norm = stats.norm()
+x_unif = norm.cdf(x)
+h = sns.jointplot(x=x_unif[:, 0], y=x_unif[:, 1], kind='scatter')
+h.set_axis_labels('Y1', 'Y2', fontsize=16)
+plt.show()
+m1 = stats.gamma(a=0.532195193066068, scale=1115.06274805659)
+m2 = stats.gamma(a=0.851708806158286, scale=763.898780284425)
+
+x1_check = m1.rvs(15000)
+x2_check = m2.rvs(15000)
+np.savetxt("x1_check.csv", x1_check, delimiter=",")
+np.savetxt("x2_check.csv", x2_check, delimiter=",")
+# plt.hist(x1,  alpha=0.5, label='x1',color='red')
+# plt.hist(x1_check,  alpha=0.5, label='marginal of x1', color='blue')
+# plt.legend(loc='upper right')
+# plt.show()
+# plt.hist(x2,  alpha=0.5, label='x2',color='red')
+# plt.hist(x2_check,  alpha=0.5, label='marginal of x2', color='blue')
+# plt.legend(loc='upper right')
+# plt.show()
+
+x1_trans = m1.ppf(x_unif[:, 0])
+x2_trans = m2.ppf(x_unif[:, 1])
+a_plus_b_trans = x1_trans + x2_trans
+np.savetxt("x1_trans.csv", x1_trans, delimiter=",")
+np.savetxt("x2_trans.csv", x2_trans, delimiter=",")
+np.savetxt("a_plus_b_trans.csv", a_plus_b_trans, delimiter=",")
+# h = sns.jointplot(x=x1_trans, y=x2_trans, kind='scatter')
+# h.set_axis_labels('X1', 'X2', fontsize=16)
+# plt.show()
+
+# Contrast that with the joint distribution without correlations
+xx1 = m1.rvs(10000)
+xx2 = m2.rvs(10000)
+np.savetxt("xx1.csv", xx1, delimiter=",")
+np.savetxt("xx2.csv", xx2, delimiter=",")
+
+# h = sns.jointplot(x=xx1, y=xx2, kind='scatter')
+# h.set_axis_labels('X1', 'X2',  fontsize=16)
+# plt.show()
+
+plt.hist(a_plus_b, alpha=0.5, label='x1+x2', color='red', weights=np.ones(len(a_plus_b)) / len(a_plus_b),
+         histtype='step')
+plt.hist(a_plus_b_trans, alpha=0.5, label='x1+x2 from copula', color='green', histtype='step', density=True,
+         weights=np.ones(len(a_plus_b_trans)) / len(a_plus_b_trans))
+plt.gca().yaxis.set_major_formatter(PercentFormatter(1))
+plt.legend(loc='upper right')
+# plt.show()
+
+
+# a_plus_b = df["A_plus_B"]
+# sns.histplot(a_plus_b ,kde=True)
+# plt.show()
+
+
+# x1 = np.log(df["ClaimNb"])
+# x2 = np.log(df["KillInjNb"])
+# x1 = df["ClaimNb"]
+# x2 = df["KillInjNb"]
 # h = sns.jointplot(x=x1, y=x2, kind='scatter', xlim=(0, 2000), ylim=(0, 2000))
 # h.set_axis_labels('X1', 'X2', fontsize=16)
 # plt.show()
@@ -31,17 +98,17 @@ x2 = df["KillInjNb"]
 # print(f.summary())
 # print(f.fitted_param["lognorm"])
 
-mvnorm = stats.multivariate_normal(mean=[0, 0], cov=[[1., 0.95],
-                                                     [0.95, 1.]])
+# mvnorm = stats.multivariate_normal(mean=[0, 0], cov=[[1., 0.95],
+#                                                      [0.95, 1.]])
 # Generate random samples from multivariate normal with correlation .5
-x = mvnorm.rvs(15000)
+# x = mvnorm.rvs(15000)
 
 # h = sns.jointplot(x=x[:, 0], y=x[:, 1], kind='scatter')
 # h.set_axis_labels('X1', 'X2', fontsize=16)
 # plt.show()
 
-norm = stats.norm()
-x_unif = norm.cdf(x)
+# norm = stats.norm()
+# x_unif = norm.cdf(x)
 # h = sns.jointplot(x= x_unif[:, 0], y= x_unif[:, 1],  kind='scatter')
 # h.set_axis_labels('Y1', 'Y2', fontsize=16)
 # plt.show()
@@ -53,19 +120,19 @@ x_unif = norm.cdf(x)
 # m1 = stats.beta(a=2.42, b=2.51, loc=1.41, scale=7.77)
 # m2 = stats.beta(a=2.11, b=2.47, loc=3.33, scale=5.44)
 # m1 = stats.lognorm(s=1.7, loc=4.11, scale=168.57)
-m1 = stats.gamma(a=0.532195193066068, scale=1115.06274805659)
-m2 = stats.gamma(a=0.851708806158286, scale=763.898780284425)
-
-x1_check = m1.rvs(15000)
-x2_check = m2.rvs(15000)
-x1_trans = m1.ppf(x_unif[:, 0])
-x2_trans = m2.ppf(x_unif[:, 1])
-a_plus_b_trans = x1_trans + x2_trans
-print(x1_trans.mean())
-print(x2_trans.mean())
-print(a_plus_b_trans.mean())
-sns.histplot(a_plus_b_trans)
-plt.show()
+# m1 = stats.gamma(a=0.532195193066068, scale=1115.06274805659)
+# m2 = stats.gamma(a=0.851708806158286, scale=763.898780284425)
+#
+# x1_check = m1.rvs(15000)
+# x2_check = m2.rvs(15000)
+# x1_trans = m1.ppf(x_unif[:, 0])
+# x2_trans = m2.ppf(x_unif[:, 1])
+# a_plus_b_trans = x1_trans + x2_trans
+# print(x1_trans.mean())
+# print(x2_trans.mean())
+# print(a_plus_b_trans.mean())
+# sns.histplot(a_plus_b_trans)
+# plt.show()
 
 # h = sns.jointplot(x=x1_trans, y=x2_trans, kind='scatter', xlim=(0, 30000), ylim=(0, 30000))
 # h.set_axis_labels('X1', 'X2', fontsize=16)
