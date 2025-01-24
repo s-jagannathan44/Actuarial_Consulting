75a71820dc0e0141a072bfc1d1606dc01aa3fe0f
s-jagannathan44 <jagannathan.s@gmail.com>
2025-01-24 07:39:56 +0530

    Creation of csv file with multiplier used for modelling

--- Client/PC_OD/PC -Analysis.py
+++ Client/PC_OD/PC -Analysis.py
@@ -1,5 +1,5 @@
 import pandas as pd
-import math
+
 
 # import duckdb as db
 
@@ -22,23 +22,34 @@ def set_financial_year(year_p):
 
 
 def apply_multiplier(quarter):
-    if quarter in ["2023_Q1", "2023_Q2", "2023_Q3", "2023_Q4"]:
-        return 1.001
+    if quarter in ["2023_Q1"]:
+        return 0.996
+    elif quarter == "2023_Q2":
+        return 1.0002
+    elif quarter == "2023_Q3":
+        return 1.0033
+    elif quarter == "2023_Q4":
+        return 1.0018
     elif quarter == "2024_Q1":
-        return 1.002
+        return 1.0071
     elif quarter == "2024_Q2":
-        return 1.003
+        return 1.0084
     elif quarter == "2024_Q3":
-        return 1.009
+        return 1.0479
     elif quarter == "2024_Q4":
-        return 1.112
+        return 1.1965
 
 
 df = pd.read_csv("CSV\\policy_claims_analysis.csv")
 df = df[df["Accident_Year"].isin([2023, 2024])]
 df['Loss Date'] = pd.to_datetime(df['Loss Date'], format="mixed", dayfirst=True)
-df["Accident_Quarter"] = df["Loss Date"].apply(lambda x: set_financial_year(x))
 df['registration_rto_code'] = df['registration_rto_code'].str.replace('DL01', 'DL1')
 df['registration_rto_code'] = df['registration_rto_code'].str.replace('RJ01', 'RJ1')
-df["PAID_AMT"] = df["total_paid"] * df["Accident_Quarter"].apply(lambda x: apply_multiplier(x))
+df["Accident_Quarter"] = df["Loss Date"].apply(lambda x: set_financial_year(x))
+df["Ultimate_PAID"] = df["total_paid"] * df["Accident_Quarter"].apply(lambda x: apply_multiplier(x))
 df.to_csv("CSV\\Ultimate.csv")
+
+# df['Report Date'] = pd.to_datetime(df['Report Date'], format="mixed", dayfirst=True)
+# df['Claim Closed Date'] = pd.to_datetime(df['Claim Closed Date'], format="mixed", dayfirst=True)
+# df["Reported_Quarter"] = df["Report Date"].apply(lambda x: set_financial_year(x))
+# df["Paid_Quarter"] = df["Claim Closed Date"].apply(lambda x: set_financial_year(x))
