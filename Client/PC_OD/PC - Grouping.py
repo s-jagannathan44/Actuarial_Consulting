8a1290267c12916b6a6b2491df363aaf50a8b9cc
s-jagannathan44 <jagannathan.s@gmail.com>
2025-02-15 09:59:30 +0530

    New Addons Merged , Grouped and Verified

--- Client/PC_OD/PC - Grouping.py
+++ Client/PC_OD/PC - Grouping.py
@@ -1,5 +1,16 @@
 import pandas as pd
 import glob
+
+attribute_col_list = (
+    "make_name  model_name variant_name vehicle_details_segment opted_kms fuel_type registration_rto_code is_cng_fitted type_of_cng_kit  seating_capacity "
+    "transmission_type cubic_capacity cc_range registered_city_name registered_state_name vehicle_age age_range is_bi_fuel_kit owner_sr lead_day_slot "
+    "t_booking  t_parent  is_health_pb_customer is_two_wheeler_pb_customer  is_travel_pb_customer is_term_life_pb_customer is_investment_pb_customer "
+    "supplier_name   policy_type previous_policy_type  is_claims_made_in_previous_policy previous_supplier_name previous_insurer_type sum_insured "
+    "new_plan_category NCB expiry_type previous_ncb idv_slot is_compulsory_pa_cover_for_owner_driver is_legal_liability_to_paid_driver is_tp_pd_liability  "
+    "is_voluntary_discount is_klr is_pg is_paid is_zd is_ep is_lpb is_coc is_pa  is_rsa is_daily_ac  is_key_rep is_ncb_pr is_windshield is_inpc "
+    "is_electrical_accessories  is_non_electrical_accessories is_bi_fuel_kit_liability is_pa_for_unnamed_passenger is_ncbprot    "
+).split()
+
 # Sum Insured has been removed as we are normalising by sum insured
 # Accident Year has been added
 master_col_list = (
@@ -7,7 +18,7 @@ master_col_list = (
     "cubic_capacity  vehicle_details_segment supplier_name  opted_kms policy_type registration_rto_code seating_capacity "
     "is_cng_fitted type_of_cng_kit cc_range previous_ncb new_plan_category NCB  age_range idv_slot is_health_pb_customer "
     "is_claims_made_in_previous_policy is_two_wheeler_pb_customer  is_travel_pb_customer is_term_life_pb_customer lead_day_slot "
-    "expiry_type is_ep is_coc   is_rsa  is_key_rep   is_inpc is_bi_fuel_kit_liability is_tp_pd_liability  t_booking  t_parent "    
+    "expiry_type is_ep is_coc   is_rsa is_klr is_ncbprot is_key_rep   is_inpc is_bi_fuel_kit_liability is_tp_pd_liability  t_booking  t_parent "
     "previous_supplier_name is_bi_fuel_kit owner_sr previous_policy_type previous_insurer_type Accident_Year"
 ).split()
 
@@ -19,7 +30,7 @@ model_col_list = (
     "vehicle_age  make_name  model_name  transmission_type  fuel_type  cubic_capacity  vehicle_details_segment  "
     "supplier_name policy_type registration_rto_code seating_capacity is_health_pb_customer revised_plan_category "
     "ncb_composite revised_is_cng_fitted is_two_wheeler_pb_customer  is_travel_pb_customer is_term_life_pb_customer "
-    "lead_day_slot expiry_type is_ep is_coc   is_rsa  is_key_rep   is_inpc t_booking  t_parent previous_supplier_name "    
+    "lead_day_slot expiry_type is_ep is_coc   is_rsa  is_key_rep   is_inpc t_booking  t_parent previous_supplier_name "
     "owner_sr previous_policy_type previous_insurer_type"
 ).split()
 
@@ -29,16 +40,17 @@ large_model_col_list = (
 
 
 def find_gamma_separation(dataframe, columns):
-    df2 = pd.pivot_table(dataframe, values="Pred_total  Normalized_LIVES_EXPOSED".split(),
+    df2 = pd.pivot_table(dataframe, values="Ultimate_PAID  Normalized_LIVES_EXPOSED".split(),
                          columns=columns,
                          aggfunc="sum").T
-    df2["LossCost"] = df2["Pred_total"] / df2["Normalized_LIVES_EXPOSED"]
+    df2["LossCost"] = df2["Ultimate_PAID"] / df2["Normalized_LIVES_EXPOSED"]
     df2.sort_values(by='Normalized_LIVES_EXPOSED', ascending=False, inplace=True)
-    df2.to_csv("Output\\Sep\\gamma_" + columns + ".csv")
+    df2.to_csv("Output\\Sep\\" + columns + ".csv")
 
 
 def find_ttl_separation(dataframe, columns):
-    df2 = pd.pivot_table(dataframe, values="ultimate_paid_large_ttl sum_insured_in_hundreds  Normalized_LIVES_EXPOSED".split(),
+    df2 = pd.pivot_table(dataframe,
+                         values="ultimate_paid_large_ttl sum_insured_in_hundreds  Normalized_LIVES_EXPOSED".split(),
                          columns=columns,
                          aggfunc="sum").T
     df2["IDV_LossCost"] = df2["ultimate_paid_large_ttl"] / df2["sum_insured_in_hundreds"]
@@ -48,7 +60,8 @@ def find_ttl_separation(dataframe, columns):
 
 
 def find_separation(dataframe, columns):
-    df2 = pd.pivot_table(dataframe, values="ultimate_paid_non_large sum_insured_in_hundreds  Normalized_LIVES_EXPOSED".split(),
+    df2 = pd.pivot_table(dataframe,
+                         values="ultimate_paid_non_large sum_insured_in_hundreds  Normalized_LIVES_EXPOSED".split(),
                          columns=columns,
                          aggfunc="sum").T
     df2["IDV_LossCost"] = df2["ultimate_paid_non_large"] / df2["sum_insured_in_hundreds"]
@@ -73,7 +86,7 @@ def correct_plan_name(x):
         return x.split("_", 1)[0]
 
 
-def convert_null_to_na():
+def convert_null_to_na(df):
     df["NCB"] = df["NCB"].astype(str)
     df["previous_ncb"] = df["previous_ncb"].astype(str)
     df["cubic_capacity"] = df["cubic_capacity"].astype(str)
@@ -100,9 +113,9 @@ def convert_null_to_na():
     df["owner_sr"] = df["owner_sr"].fillna("-1")
 
 
-df = pd.read_csv("Output\\ModelGammaInput.csv")
-df["Pred_total"] = df["Pred_Cost_NL"] + df["Pred_Cost_ttl"]
-df.drop(["Unnamed: 0", "Unnamed: 0.1"], axis=1, inplace=True)
+# df = pd.read_csv("CSV\\FixedMultiplier\\Combined_final_file.csv")
+#
+# df.drop(["Unnamed: 0"], axis=1, inplace=True)
 # convert_null_to_na()
 #
 # df["revised_is_cng_fitted"] = df["is_cng_fitted"].apply(lambda x: "Kit_Is" if x > 0 else "No_Kit")
@@ -121,15 +134,71 @@ df.drop(["Unnamed: 0", "Unnamed: 0.1"], axis=1, inplace=True)
 #         axis=1, inplace=True)
 
 
-for var in model_col_list:
-    find_gamma_separation(df, var)
+# for var in master_col_list:
+#     find_gamma_separation(df, var)
+#
+# path = "Output/Sep/*.csv"
+# files = glob.glob(path)
+# writer = pd.ExcelWriter('Output\\Previous_Clubbing.xlsx')
+# for file_name in files:
+#     frame = pd.read_csv(file_name)
+#     frame.to_excel(writer, sheet_name=file_name[11:-4])
+# writer.close()
 
-path = "Output/Sep/*.csv"
+
+def find_old_separation(dataframe, columns):
+    df2 = pd.pivot_table(dataframe, values="Ultimate_PAID  Normalized_LIVES_EXPOSED".split(),
+                         columns=columns,
+                         aggfunc="sum").T
+    df2["LossCost"] = df2["Ultimate_PAID"] / df2["Normalized_LIVES_EXPOSED"]
+    df2.sort_values(by='Normalized_LIVES_EXPOSED', ascending=False, inplace=True)
+    df2.to_csv("Output\\Sep\\Old\\" + columns + ".csv")
+
+
+def find_new_separation(dataframe, columns):
+    df2 = pd.pivot_table(dataframe, values="Ultimate_PAID  Normalized_LIVES_EXPOSED".split(),
+                         columns=columns,
+                         aggfunc="sum").T
+    df2["LossCost"] = df2["Ultimate_PAID"] / df2["Normalized_LIVES_EXPOSED"]
+    df2.sort_values(by='Normalized_LIVES_EXPOSED', ascending=False, inplace=True)
+    df2.to_csv("Output\\Sep\\New\\" + columns + ".csv")
+
+
+# old = pd.read_csv("CSV\\FixedMultiplier\\Combined_final_file.csv")
+# new = pd.read_csv("Output\\Combined_final_file.csv")
+# old.drop(["Unnamed: 0"], axis=1, inplace=True)
+# convert_null_to_na(old)
+# new.drop(["Unnamed: 0"], axis=1, inplace=True)
+# convert_null_to_na(new)
+#
+# for var in attribute_col_list:
+#     find_new_separation(new, var)
+#
+col_list = [item for item in attribute_col_list if item != "is_klr"]
+old_list = [item for item in col_list if item != "is_ncbprot"]
+#
+# for var in old_list:
+#     find_old_separation(old, var)
+
+for column in old_list:
+    new = pd.read_csv("Output\\Sep\\Old\\" + column + ".csv")
+    old = pd.read_csv("Output\\Sep\\New\\" + column + ".csv")
+    new.drop(["Ultimate_PAID"], axis=1, inplace=True)
+    old.drop(["Ultimate_PAID"], axis=1, inplace=True)
+    old.rename(columns={"Normalized_LIVES_EXPOSED": "Exposure_Old"}, inplace=True)
+    new.rename(columns={"Normalized_LIVES_EXPOSED": "Exposure_new"}, inplace=True)
+    old.rename(columns={"LossCost": "LossCost_Old"}, inplace=True)
+    new.rename(columns={"LossCost": "LossCost_New"}, inplace=True)
+
+    df_final = old.merge(new, on=[column], how="left")
+    df_final["Diff"] = df_final["Exposure_new"] - df_final["Exposure_Old"]
+    df_final.to_csv("Output\\Sep\\final\\" + column + ".csv")
+
+path = "Output/Sep/final/*.csv"
 files = glob.glob(path)
-writer = pd.ExcelWriter('Output\\Gamma_Clubbing.xlsx')
+writer = pd.ExcelWriter('Output\\Diff_Clubbing.xlsx')
 for file_name in files:
     frame = pd.read_csv(file_name)
-    frame.to_excel(writer, sheet_name=file_name[11:-4])
+    frame.drop(["Unnamed: 0"], axis=1, inplace=True)
+    frame.to_excel(writer, sheet_name=file_name[17:-4])
 writer.close()
-
-
--- Client/PC_OD/PC -Analysis.py
+++ Client/PC_OD/PC -Analysis.py
@@ -1,6 +1,4 @@
 import pandas as pd
-
-
 # import glob
 
 
@@ -17,139 +15,103 @@ def set_financial_year(year_p):
         return ""
 
 
-def apply_non_large_multiplier(quarter):
+def apply_multiplier(quarter):
     if quarter in ["2023_Q1"]:
-        return 1.000
+        return 0.996
     elif quarter == "2023_Q2":
-        return 1.000
-    elif quarter == "2023_Q3":
-        return 1.000
-    elif quarter == "2023_Q4":
         return 1.0002
-    elif quarter == "2024_Q1":
-        return 1.0006
-    elif quarter == "2024_Q2":
-        return 1.0001
-    elif quarter == "2024_Q3":
-        return 1.0020
-    elif quarter == "2024_Q4":
-        return 1.0214
-
-
-def apply_large_multiplier(quarter):
-    if quarter in ["2023_Q1"]:
-        return 1.000
-    elif quarter == "2023_Q2":
-        return 1.0115
     elif quarter == "2023_Q3":
-        return 1.0023
+        return 1.0033
     elif quarter == "2023_Q4":
-        return 1.0294
+        return 1.0018
     elif quarter == "2024_Q1":
-        return 1.0483
+        return 1.0071
     elif quarter == "2024_Q2":
-        return 1.0248
+        return 1.0084
     elif quarter == "2024_Q3":
-        return 1.0228
+        return 1.0479
     elif quarter == "2024_Q4":
-        return 1.1337
+        return 1.1965
 
 
-def apply_theft_tl_multiplier(quarter):
-    if quarter in ["2023_Q1"]:
-        return 1.00
-    elif quarter == "2023_Q2":
-        return 1.000
-    elif quarter == "2023_Q3":
-        return 1.00
-    elif quarter == "2023_Q4":
-        return 1.00
-    elif quarter == "2024_Q1":
-        return 1.00
-    elif quarter == "2024_Q2":
-        return 1.00
-    elif quarter == "2024_Q3":
-        return 1.0353
-    elif quarter == "2024_Q4":
-        return 1.3999
-
-
-def split_into_3(frame):
-    theft_tl_ = frame[frame["cause_of_loss"].isin(
-        ["Theft of entire vehicle", "Theft", "Theft of Entire Vehicle", "THEFT-VEHICLE", "Theft - Vehicle",
-         "Theft of Vehicle", "Total Loss"])]
-    non_theft = frame[~ frame["cause_of_loss"].str.contains("Theft of entire vehicle", na=False)]
-    non_theft = non_theft[~ non_theft["cause_of_loss"].str.contains("Theft", na=False)]
-    non_theft = non_theft[~ non_theft["cause_of_loss"].str.contains("Theft of Entire Vehicle", na=False)]
-    non_theft = non_theft[~ non_theft["cause_of_loss"].str.contains("THEFT-VEHICLE", na=False)]
-    non_theft = non_theft[~ non_theft["cause_of_loss"].str.contains("Theft - Vehicle", na=False)]
-    non_theft = non_theft[~ non_theft["cause_of_loss"].str.contains("Theft of Vehicle", na=False)]
-    non_theft = non_theft[~ non_theft["cause_of_loss"].str.contains("Total Loss", na=False)]
-    large_ = non_theft[non_theft["is_non_large"] == 'N']
-    non_large_ = non_theft[non_theft["is_non_large"] == 'Y']
-    return theft_tl_, large_, non_large_
-
-
-def check_cause_of_loss(x):
-    if "Theft of entire vehicle" in x or "Theft" in x or "Theft of Entire Vehicle" in x or "THEFT-VEHICLE" in x \
-            or "Theft - Vehicle" in x or "Theft of Vehicle" in x or "Total Loss" in x:
-        return 1
-    else:
-        return 0
+# df = pd.read_csv("CSV\\policy_claims_analysis.csv")
+# df = df[df["Accident_Year"].isin([2023, 2024])]
+# df['Loss Date'] = pd.to_datetime(df['Loss Date'], format="mixed", dayfirst=True)
+# df['registration_rto_code'] = df['registration_rto_code'].str.replace('DL01', 'DL1')
+# df['registration_rto_code'] = df['registration_rto_code'].str.replace('RJ01', 'RJ1')
+# df["Accident_Quarter"] = df["Loss Date"].apply(lambda x: set_financial_year(x))
+# df["Ultimate_PAID"] = df["total_paid"] * df["Accident_Quarter"].apply(lambda x: apply_multiplier(x))
+# df.to_csv("CSV\\Ultimate.csv")
 
 
 # df = pd.read_csv("CSV\\Ultimate_fixed.csv")
-# df["IDV_Ratio"] = df["incurred"] / df["sum_insured"]
-# df["is_non_large"] = df["IDV_Ratio"].apply(lambda x: "Y" if x <= 0.4 else "N")
-#
-# # Step 1 remove 3 problematic companies
-# df = df[~ df["supplier_name"].str.contains("Iffco Tokio General Insurance Company Ltd", na=False)]
-# df = df[~ df["supplier_name"].str.contains("Raheja QBE General Insurance Company", na=False)]
-# df = df[~ df["supplier_name"].str.contains("SBI General Insurance Company Ltd", na=False)]
-#
-# # Step 2 Split  file into Non-large, large , theft and total loss
-# theft_tl, large, non_large = split_into_3(df)
-#
-# # Step 3 apply multipliers
-#
-# theft_tl["Ultimate_PAID"] = theft_tl["total_paid"] * theft_tl["Accident_Quarter"].apply(
-#     lambda x: apply_theft_tl_multiplier(x))
-# theft_tl.to_csv("CSV\\FixedMultiplier\\theft_total_loss.csv")
-#
-# large["Ultimate_PAID"] = large["total_paid"] * large["Accident_Quarter"].apply(
-#     lambda x: apply_large_multiplier(x))
-# large.to_csv("CSV\\FixedMultiplier\\large.csv")
-#
-# non_large["Ultimate_PAID"] = non_large["total_paid"] * non_large["Accident_Quarter"].apply(
-#     lambda x: apply_non_large_multiplier(x))
-# non_large.to_csv("CSV\\FixedMultiplier\\non_large.csv")
+# missing = pd.read_csv("CSV\\Iffco Missing Data.csv")
+# df.drop(["Unnamed: 0.1", "Unnamed: 0", ], axis=1, inplace=True)
+# missing.drop(['lead_id', 'bookingdate'], axis=1, inplace=True)
+# df_final = df.merge(missing, on=["Policy_Number"], how="left")
+# df_final.to_csv("Output\\Missing_merged.csv")
+# df = pd.read_csv("Output\\Missing_merged.csv")
+# iffco = pd.read_csv("Output\\Iffco.csv")
+# df.drop(["Unnamed: 0", ], axis=1, inplace=True)
+# # df[df["supplier_name"]== "Iffco Tokio General Insurance Company Ltd"].to_csv("Output\\Iffco.csv")
+# df = df[~ df["supplier_name"].str.contains("Iffco Tokio General Insurance Company Ltd")]
+# df = pd.concat([df, iffco], axis=0)
+# df.drop(["t_booking", "t_parent", "is_paid", "NCB", "previous_ncb"], axis=1, inplace=True)
+# df.rename(columns={'t_booking_y': "t_booking", 't_parent_y': "t_parent",
+#                    'is_paid_y': 'is_paid', 'NCB_y': 'NCB', 'previous_ncb_y': 'previous_ncb'
+#                    })
+# df.to_csv("CSV\\Ultimate_fixed.csv")
 
-# df1 = pd.read_csv("CSV\\FixedMultiplier\\non_large.csv")
-# df2 = pd.read_csv("CSV\\FixedMultiplier\\large.csv")
-# df3 = pd.read_csv("CSV\\FixedMultiplier\\theft_total_loss.csv")
-# df = pd.concat([df1, df2, df3], axis=0)
-# df.to_csv("CSV\\FixedMultiplier\\Combined_file.csv")
-# df = pd.read_csv("CSV\\FixedMultiplier\\Combined_new_file.csv")
-# # df["cause_of_loss"] = df["cause_of_loss"].fillna("NA")
-# # df["is_theft_total_loss"] = df["cause_of_loss"].apply(lambda x: check_cause_of_loss(x))
-# # df["is_large_loss_non_theft"] = df["IDV_Ratio"].apply(lambda x: 0 if x <= 0.4 else 1) - df["is_theft_total_loss"]
-# # df["is_large_loss_non_theft"] = df["is_large_loss_non_theft"].apply(lambda x: 0 if x < 0 else x)
-# # df["non_theft_non_large"] = df["IDV_Ratio"].apply(lambda x: 1 if x <= 0.4 else 0) - df["is_theft_total_loss"]
-# # df["non_theft_non_large"] = df["non_theft_non_large"].apply(lambda x: 0 if x < 0 else x)
-# # df["ultimate_paid_non_large"] = df["Ultimate_PAID"] * df["non_theft_non_large"]
-# # df["sum_insured_in_hundreds"] = df["sum_insured"]/1000
-# df["large_theft_total_loss"] = df["is_large_loss_non_theft"] + df["is_theft_total_loss"]
-# df["large_theft_total_loss"] = df["large_theft_total_loss"].apply(lambda x: 1 if x > 1 else x)
+
+# def merge_ncb():
+#     path = "CSV/NCB/ncb*.csv"
+#     df = pd.DataFrame()
+#     files = glob.glob(path)
+#     for file_name in files:
+#         frame = pd.read_csv(file_name)
+#         df = pd.concat([df, frame], axis=0)
+#     df.to_csv("CSV\\NCB\\NCB_file.csv")
 #
-# df["ultimate_paid_large_ttl"] = df["Ultimate_PAID"] * df["large_theft_total_loss"]
 #
-# df.to_csv("CSV\\FixedMultiplier\\Combined_ttl_file.csv")
+# def merge_booking():
+#     path = "CSV/NCB/t*.csv"
+#     df = pd.DataFrame()
+#     files = glob.glob(path)
+#     for file_name in files:
+#         frame = pd.read_csv(file_name)
+#         df = pd.concat([df, frame], axis=0)
+#     df.to_csv("CSV\\NCB\\t_booking_file.csv")
+#
+
+# ncb = pd.read_csv("CSV\\NCB\\NCB_file.csv")
+# booking = pd.read_csv("CSV\\NCB\\t_booking_file.csv")
+# ncb.drop(["lead_id", "bookingdate", "Unnamed: 0"], axis=1, inplace=True)
+# booking.drop(["lead_id", "bookingdate", "Unnamed: 0"], axis=1, inplace=True)
+# ncb.rename(columns={"policy_no": "Policy_Number"}, inplace=True)
+# booking.rename(columns={"policy_no": "Policy_Number"}, inplace=True)
+# df_final = ncb.merge(booking, on=["Policy_Number"], how="left")
+# df_final.to_csv("CSV\\Consolidated_fixed.csv")
+
+# pol = pd.read_csv("CSV\\Ultimate.csv")
+# fixed = pd.read_csv("CSV\\Consolidated_fixed.csv")
+# pol.drop(["Unnamed: 0.1", "Unnamed: 0"], axis=1, inplace=True)
+# fixed.drop(["Unnamed: 0"], axis=1, inplace=True)
+# df_final = pol.merge(fixed, on=["Policy_Number"], how="left")
+# df_final.to_csv("CSV\\Ultimate_fixed.csv")
+
+# path = "CSV/Addons/*.csv"
+# df = pd.DataFrame()
+# files = glob.glob(path)
+# for file_name in files:
+#     frame = pd.read_csv(file_name)
+#     df = pd.concat([df, frame], axis=0)
+# df.to_csv("CSV\\Addons\\addons_file.csv")
 
 df = pd.read_csv("CSV\\FixedMultiplier\\Combined_final_file.csv")
-missing = pd.read_csv("CSV\\base_file.csv", usecols=["policy_no", "previous_insurer_type"])
-missing.rename(columns={"policy_no": "Policy_Number"}, inplace=True)
-df.drop(["Unnamed: 0", "Unnamed: 0.1", "Unnamed: 0.2", "Unnamed: 0.3", "Unnamed: 0.4",  "Unnamed: 0.5"], axis=1, inplace=True)
-df_final = df.merge(missing, on=["Policy_Number"], how="left")
+addons = pd.read_csv("CSV\\Addons\\addons_file.csv")
+addons.rename(columns={"policy_no": "Policy_Number"}, inplace=True)
+addons = addons.drop_duplicates(subset=['Policy_Number'])
+addons.drop(["lead_id", "bookingdate", "Unnamed: 0"], axis=1, inplace=True)
+df.drop(["Unnamed: 0", "is_lpb", "is_coc", "is_ep", "is_zd",  "is_rsa", "is_inpc"], axis=1, inplace=True)
+df_final = df.merge(addons, on=["Policy_Number"], how="left")
 df_final.to_csv("Output\\Combined_final_file.csv")
-
-
